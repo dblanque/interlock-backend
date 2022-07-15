12f03afcf0c2e0d8ccd3e8b96e16d4a0aeb449ba
dblanque <dylan.blanque@gmail.com>
2022-07-15 15:27:52 -0300

    Added UserDoesNotExist exception

--- core/exceptions/users.py
+++ core/exceptions/users.py
@@ -5,18 +5,19 @@ class UserExists(APIException):
     status_code = 520
     default_detail = 'User already exists'
     default_code = 'user_exists'
-
 class UserPermissionError(APIException):
     status_code = 521
     default_detail = 'User permissions in request are malformed'
     default_code = 'user_permission_malformed'
-
 class UserPasswordsDontMatch(APIException):
     status_code = 522
     default_detail = 'User passwords do not match'
     default_code = 'user_passwords_dont_match'
-
 class UserUpdateError(APIException):
     status_code = 523
     default_detail = 'User could not be updated'
     default_code = 'user_update_error'
+class UserDoesNotExist(APIException):
+    status_code = 524
+    default_detail = 'User Distinguished Name does not exist'
+    default_code = 'user_dn_does_not_exist'
