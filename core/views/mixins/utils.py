91b00791a0770b2e226ee0b5ddf2a6429a4c96e2
dblanque <dylan.blanque@gmail.com>
2022-07-26 18:03:34 -0300

    Added a couple of test functions

--- core/views/mixins/utils.py
+++ core/views/mixins/utils.py
@@ -10,3 +10,20 @@ def testPort(ip , port, timeout=5):
     return True
   except:
     return False
+
+def recursiveFindInDict(obj, key):
+  if key in obj: return obj[key]
+  for k, v in obj.items():
+      if isinstance(v,dict):
+          item = recursiveFindInDict(v, key)
+          if item is not None:
+              return item
+
+# Check if in current level array
+# check if has children
+# if has children check in children array
+# if children have children call itself
+
+def testFunc(*args):
+  for i in args:
+    print(i)
