00ff4f82cd1ac0876b2eee948667db72348e0058
dblanque <dylan.blanque@gmail.com>
2022-08-08 12:49:08 -0300

    Added DNS Listing

--- core/models/dns.py
+++ core/models/dns.py
@@ -2,8 +2,61 @@ from core.utils.dns import *
 from core.utils import dnstool
 from interlock_backend.ldap.settings_func import SettingsList
 import ipaddress
+import logging
 
-class ldapDNS():
+logger = logging.getLogger(__name__)
+
+def record_to_dict(record, ts=False):
+    # For original reference see print_record in dnstool.py
+    try:
+        rtype = dnstool.RECORD_TYPE_MAPPING[record['Type']]
+    except KeyError:
+        rtype = 'Unsupported'
+
+    record_dict = dict()
+
+    # Check if record is Tombstoned / Inactive
+    if ts:
+        record_dict['ts'] = True
+    else:
+        record_dict['ts'] = False
+
+    record_dict['type'] = record['Type']
+    record_dict['typeName'] = rtype
+    record_dict['serial'] = record['Serial']
+    if record['Type'] == 0:
+        tstime = dnstool.DNS_RPC_RECORD_TS(record['Data'])
+        record_dict['tstime'] = tstime.toDatetime()
+    # A record
+    if record['Type'] == 1:
+        address = dnstool.DNS_RPC_RECORD_A(record['Data'])
+        record_dict['address'] = address.formatCanonical()
+    # NS record or CNAME record
+    if record['Type'] == 2 or record['Type'] == 5:
+        address = dnstool.DNS_RPC_RECORD_NODE_NAME(record['Data'])
+        record_dict['address'] = address['nameNode'].toFqdn()
+    # SRV record
+    if record['Type'] == 33:
+        record_data = dnstool.DNS_RPC_RECORD_SRV(record['Data'])
+        record_dict['wPriority'] = record_data['wPriority']
+        record_dict['wWeight'] = record_data['wWeight']
+        record_dict['wPort'] = record_data['wPort']
+        record_dict['nameTarget'] = record_data['nameTarget'].toFqdn()
+    # SOA record
+    if record['Type'] == 6:
+        record_data = dnstool.DNS_RPC_RECORD_SOA(record['Data'])
+        record_dict['dwSerialNo'] = record_data['dwSerialNo']
+        record_dict['dwRefresh'] = record_data['dwRefresh']
+        record_dict['dwRetry'] = record_data['dwRetry']
+        record_dict['dwExpire'] = record_data['dwExpire']
+        record_dict['dwMinimumTtl'] = record_data['dwMinimumTtl']
+        record_dict['namePrimaryServer'] = record_data['namePrimaryServer'].toFqdn()
+        record_dict['zoneAdminEmail'] = record_data['zoneAdminEmail'].toFqdn()
+
+    return record_dict
+    # record_data.dump()
+
+class LDAPDNS():
     def __init__(self, connection, legacy=False):
         ######################## Get Latest Settings ###########################
         ldap_settings_list = SettingsList(**{"search":{
@@ -21,15 +74,15 @@ class ldapDNS():
     def list_dns_zones(self):
         zones = dnstool.get_dns_zones(self.connection, self.dnsroot)
         if len(zones) > 0:
-            print('Found %d domain DNS zones:' % len(zones))
+            logger.info('Found %d domain DNS zone(s):' % len(zones))
             for zone in zones:
-                print('    %s' % zone)
+                logger.info('    %s' % zone)
         return zones
 
-    def list_dns_zones(self):
+    def list_forest_zones(self):
         zones = dnstool.get_dns_zones(self.connection, self.forestroot)
         if len(zones) > 0:
-            print('Found %d forest DNS zones:' % len(zones))
+            logger.info('Found %d forest DNS zone(s):' % len(zones))
             for zone in zones:
-                print('    %s' % zone)
+                logger.info('    %s' % zone)
         return zones
--- core/views/domain.py
+++ core/views/domain.py
@@ -1,21 +1,31 @@
 ################################## IMPORTS #####################################
+### ViewSets
+from email import header
+from core.views.base import BaseViewSet
+
 ### Exceptions
 from django.core.exceptions import PermissionDenied
+from core.exceptions import (
+    ldap as exc_ldap,
+)
 
 ### Mixins
 from .mixins.domain import DomainViewMixin
 
 ### REST Framework
 from rest_framework.response import Response
-from rest_framework import viewsets
 from rest_framework.exceptions import NotFound
 from rest_framework.decorators import action
 
 ### Others
+from interlock_backend.ldap.adsi import addSearchFilter
 from interlock_backend.ldap.encrypt import validateUser
 from interlock_backend.ldap.settings_func import SettingsList
+from interlock_backend.ldap.connector import LDAPConnector
+from core.utils import dnstool
+from core.models.dns import LDAPDNS, record_to_dict
 ################################################################################
-class DomainViewSet(viewsets.ViewSet, DomainViewMixin):
+class DomainViewSet(BaseViewSet, DomainViewMixin):
 
     @action(detail=False, methods=['get'])
     def details(self, request):
@@ -35,29 +45,106 @@ class DomainViewSet(viewsets.ViewSet, DomainViewMixin):
              }
         )
 
-    def list(self, request, pk=None):
-        raise NotFound
+    @action(detail=False, methods=['post'])
+    def zones(self, request):
+        user = request.user
+        validateUser(request=request, requestUser=user, requireAdmin=False)
+        data = {}
+        code = 0
 
-    def create(self, request, pk=None):
-        raise NotFound
+        reqData = request.data
 
-    def put(self, request, pk=None):
-        raise NotFound
+        if 'filter' in reqData:
+            if 'dnsZone' in reqData['filter']:
+                zoneFilter = str(reqData['filter']['dnsZone']).replace(" ", "")
 
-    def patch(self, request, pk=None):
-        raise NotFound
-        
-    def retrieve(self, request, pk=None):
-        raise NotFound
+        if zoneFilter is not None:
+            if zoneFilter == "" or len(zoneFilter) == 0:
+                zoneFilter = None
 
-    def update(self, request, pk=None):
-        raise NotFound
+        ######################## Get Latest Settings ###########################
+        ldap_settings_list = SettingsList(**{"search":{
+            'LDAP_DOMAIN',
+            'LDAP_AUTH_SEARCH_BASE',
+            'LDAP_LOG_READ'
+        }})
 
-    def partial_update(self, request, pk=None):
-        raise NotFound
+        # Open LDAP Connection
+        try:
+            connector = LDAPConnector()
+            ldapConnection = connector.connection
+        except Exception as e:
+            print(e)
+            raise exc_ldap.CouldNotOpenConnection
 
-    def destroy(self, request, pk=None):
-        raise NotFound
+        responseData = {}
 
-    def delete(self, request, pk=None):
-        raise NotFound
+        responseData['headers'] = [
+            'nameTarget',
+            'address',
+            # 'type',
+            'typeName',
+            'serial',
+            'ts',
+            # 'tstime',
+            # 'wPriority',
+            # 'wWeight',
+            # 'wPort',
+            # 'dwSerialNo',
+            # 'dwRefresh',
+            # 'dwRetry',
+            # 'dwExpire',
+            # 'dwMinimumTtl',
+            # 'namePrimaryServer',
+            # 'zoneAdminEmail',
+        ]
+
+        searchFilter = addSearchFilter("", "objectClass=dnsNode")
+        attributes=['dnsRecord','dNSTombstoned','name']
+
+        dnsList = LDAPDNS(ldapConnection)
+        dnsZones = dnsList.list_dns_zones()
+        forestZones = dnsList.list_forest_zones()
+
+        if zoneFilter is not None:
+            target_zone = zoneFilter
+        else:
+            target_zone = ldap_settings_list.LDAP_DOMAIN
+        search_target = 'DC=%s,%s' % (target_zone, dnsList.dnsroot)
+        ldapConnection.search(
+            search_base=search_target,
+            search_filter=searchFilter,
+            attributes=attributes
+            )
+
+        result = list()
+
+        for entry in ldapConnection.response:
+            for record in entry['raw_attributes']['dnsRecord']:
+                dr = dnstool.DNS_RECORD(record)
+                record_dict = record_to_dict(dr, entry['attributes']['dNSTombstoned'])
+                result.append(record_dict)
+
+        ldapConnection.unbind()
+
+        cleanDnsZones = list()
+        c = 0
+        for i in dnsZones:
+            zoneName = dnsZones[c][0]
+            if zoneName == 'RootDNSServers':
+                cleanDnsZones.append("Root DNS Servers")
+            else:
+                cleanDnsZones.append(dnsZones[c][0])
+            c += 1
+
+        responseData['dnsZones'] = cleanDnsZones
+        responseData['forestZones'] = forestZones
+        responseData['records'] = result
+
+        return Response(
+             data={
+                'code': code,
+                'code_msg': 'ok',
+                'data' : responseData
+             }
+        )
