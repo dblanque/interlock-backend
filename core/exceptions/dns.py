77d2462deeaf52574c997e1a3ed96fe4c836ecf1
dblanque <dylan.blanque@gmail.com>
2022-08-09 15:18:53 -0300

    Added DNS Exceptions

--- core/exceptions/dns.py
+++ core/exceptions/dns.py
@@ -1,3 +1,20 @@
 from core.exceptions.base import BaseException
 
 # DNS Custom Exceptions
+
+class DNSZoneNotInRequest(BaseException):
+    status_code = 400
+    default_detail = 'Could not find a valid DNS Zone value'
+    default_code = 'dns_zone_missing'
+class DNSZoneInRecord(BaseException):
+    status_code = 400
+    default_detail = 'Target Record should not include the zone'
+    default_code = 'dns_zone_in_record'
+class DNSRecordNotInRequest(BaseException):
+    status_code = 400
+    default_detail = 'DNS Record is not present in request'
+    default_code = 'dns_record_not_in_request'
+class DistinguishedNameNotInDNSRecord(BaseException):
+    status_code = 400
+    default_detail = 'Distinguished Name is not present in Record'
+    default_code = 'dns_dn_not_in_record'
