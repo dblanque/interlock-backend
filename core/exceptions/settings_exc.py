182a3c66117e8c4b669bb42fb97040a7c3af06ee
dblanque <dylan.blanque@gmail.com>
2022-08-09 22:19:03 -0300

    Add exception SettingNotInList

--- core/exceptions/settings_exc.py
+++ core/exceptions/settings_exc.py
@@ -5,3 +5,8 @@ class SettingTypeDoesNotMatch(BaseException):
     status_code = 400
     default_detail = 'The Setting Type does not match with the back-end data'
     default_code = 'setting_type_malformed'
+
+class SettingNotInList(BaseException):
+    status_code = 400
+    default_detail = 'The Requested Setting is not in the current search list'
+    default_code = 'setting_not_in_list'
