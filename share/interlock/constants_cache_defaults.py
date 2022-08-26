84751f97807b5fc9a1853b6f7550c80b486280df
dblanque <dylan.blanque@gmail.com>
2022-08-26 17:37:41 -0300

    Added constants_cache default to share

---
+++ share/interlock/constants_cache_defaults.py
@@ -0,0 +1,14 @@
+# This file is generated automatically by Interlock when saving settings
+# Manual changes to it might be lost
+################################################################################
+#################### INTERLOCK IS LICENSED UNDER GNU GPLv3 #####################
+################## ORIGINAL PROJECT CREATED BY DYLAN BLANQUÉ ###################
+########################## AND BR CONSULTING S.R.L. ############################
+################################################################################
+# Module: constants_cache.py
+# Contains the latest setting constants for Interlock
+
+#---------------------------------- IMPORTS -----------------------------------#
+from interlock_backend.ldap.constants import *
+import ssl
+################################################################################
