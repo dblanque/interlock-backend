f9ad40c2223c7157e887c10387d24f621d1ec38a
dblanque <dylan.blanque@gmail.com>
2023-07-26 14:35:48 -0300

    Add LDAPConnectionNotOpen Exception

--- core/exceptions/ldap.py
+++ core/exceptions/ldap.py
@@ -6,6 +6,10 @@ class LDAPObjectExists(BaseException):
     status_code = 409
     default_detail = 'An object with this Common Name already exists'
     default_code = 'ldap_obj_exists'
+class LDAPConnectionNotOpen(BaseException):
+    status_code = 500
+    default_detail = 'No LDAP Connection was open prior to this operation'
+    default_code = 'ldap_connection_not_open'
 
 class LDAPPermissionsInsufficient(BaseException):
     status_code = 400
