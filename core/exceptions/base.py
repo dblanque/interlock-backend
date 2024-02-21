3dfc0b8918bbe56ed7099fddbec7000781fe0ae9
dblanque <dylan.blanque@gmail.com>
2024-02-21 17:36:18 -0300

    Exception Improvements. Change exceptions to use DRF Constants for HTTP Codes. Remove unused code from exceptionhandler.

--- core/exceptions/base.py
+++ core/exceptions/base.py
@@ -1,4 +1,5 @@
 from rest_framework.exceptions import APIException
+from rest_framework import status
 
 class BaseException(APIException):
     def __init__(self, data=None):
@@ -19,11 +20,31 @@ class BaseException(APIException):
             if 'detail' not in self.detail:
                 self.detail['detail'] = self.default_detail
 
+class AccessTokenInvalid(BaseException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = 'Access Token Invalid'
+    default_code = 'access_token_invalid'
+class RefreshTokenExpired(BaseException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = 'Refresh Token Expired'
+    default_code = 'refresh_token_expired'
+class BadRequest(BaseException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = 'Bad Request'
+    default_code = 'bad_request'
+class Unauthorized(BaseException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = 'Unauthorized'
+    default_code = 'unauthorized'
+class PermissionDenied(BaseException):
+    status_code = status.HTTP_403_FORBIDDEN
+    default_detail = 'Permission Denied'
+    default_code = 'permission_denied'
 class MissingDataKey(BaseException):
-    status_code = 500
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     default_detail = 'Missing key in data'
     default_code = 'data_key_missing'
 class LDAPServerUnreachable(BaseException):
-    status_code = 502
+    status_code = status.HTTP_502_BAD_GATEWAY
     default_detail = 'LDAP Server Unreachable'
     default_code = 'ldap_unreachable'
--- core/system/exceptionhandler.py
+++ core/system/exceptionhandler.py
@@ -1,6 +1,4 @@
 from rest_framework.views import exception_handler
-from rest_framework.response import Response
-from core.system.responsehandler import ResponseHandler
 
 def custom_exception_handler(exc, context):
     # Call REST framework's default exception handler first,
@@ -12,54 +10,3 @@ def custom_exception_handler(exc, context):
         response.data['status_code'] = response.status_code
 
     return response
-
-# def exception_handler(exc, context):
-#     """
-#     Returns the response that should be used for any given exception.
-#     By default we handle the REST framework `APIException`, and also
-#     Django's built-in `Http404` and `PermissionDenied` exceptions.
-#     Any unhandled exceptions may return `None`, which will cause a 500 error
-#     to be raised.
-#     """
-#     if isinstance(exc, Http404):
-#         exc = exceptions.NotFound()
-#     elif isinstance(exc, PermissionDenied):
-#         exc = exceptions.PermissionDenied()
-
-#     if isinstance(exc, exceptions.APIException):
-#         headers = {}
-#         if getattr(exc, 'auth_header', None):
-#             headers['WWW-Authenticate'] = exc.auth_header
-#         if getattr(exc, 'wait', None):
-#             headers['Retry-After'] = '%d' % exc.wait
-
-#         if isinstance(exc.detail, (list, dict)):
-#             data = exc.detail
-#         else:
-#             data = {'detail': exc.detail}
-
-#         set_rollback()
-#         return Response(data, status=exc.status_code, headers=headers)
-
-#     return None
-
-# def custom_exception_handler(exc, context):
-#     # Call REST framework's default exception handler first,
-#     # to get the standard error response.
-#     original_response = exception_handler(exc, context)
-
-#     print(original_response.data.values())
-
-#     original_response_code = next(iter(original_response.data.values()))[0].code
-#     original_fields = ', '.join(original_response.data.keys())
-#     code = None
-#     details = None
-#     # print(og_response_code)
-#     # Now add the HTTP status code to the response.
-#     if original_response_code == 'required':
-#         code = 'MISSING_FIELD'
-#         details = original_fields
-
-#     return Response(
-#         data = ResponseHandler.send(code, details)
-#     )
