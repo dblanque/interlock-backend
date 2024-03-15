1ff5825e31092d4301c9e61ec3dcb94dc1affd07
dblanque <dylan.blanque@gmail.com>
2024-03-15 01:42:00 -0300

    Add user exceptions

--- core/exceptions/users.py
+++ core/exceptions/users.py
@@ -66,3 +66,15 @@ class BadGroupSelection(BaseException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = 'The same groups are in the Add to Group and Remove from Group entries'
     default_code = 'user_group_bad'
+class UserWithEmailExists(BaseException):
+    status_code = status.HTTP_409_CONFLICT
+    default_detail = 'A user with this email already exists'
+    default_code = 'user_email_exists'
+class UserExists(BaseException):
+    status_code = status.HTTP_409_CONFLICT
+    default_detail = 'A user with this identifier already exists'
+    default_code = 'user_exists'
+class UserNotSynced(BaseException):
+    status_code = status.HTTP_404_NOT_FOUND
+    default_detail = 'No synced user with that name, make sure the user has logged into Interlock at least once.'
+    default_code = 'user_not_synced'
