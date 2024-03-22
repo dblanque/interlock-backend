ec9d145a8c8c36bfa73a836a2033e98b7e0d9774
dblanque <dylan.blanque@gmail.com>
2024-03-21 22:36:17 -0300

    Add LDAP Object Rename Exception.

--- core/exceptions/dirtree.py
+++ core/exceptions/dirtree.py
@@ -15,6 +15,10 @@ class DirtreeMove(CoreException):
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     default_detail = 'LDAP Object could not be moved'
     default_code = 'dirtree_move_error'
+class DirtreeRename(CoreException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    default_detail = 'LDAP Object could not be renamed'
+    default_code = 'dirtree_rename_error'
 class DirtreeNewNameIsOld(CoreException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = 'LDAP Object could not be moved'
