278a93dd3d8534eb2b2bfb4227f516513f989045
Dylan Blanque <dylan.blanque@gmail.com>
2025-02-27 22:59:30 -0300

    Add ApplicationExists exception.

--- core/exceptions/application.py
+++ core/exceptions/application.py
@@ -2,3 +2,8 @@ from core.exceptions.base import CoreException
 from rest_framework import status
 
 # Application Custom Exceptions
+
+class ApplicationExists(CoreException):
+    status_code = status.HTTP_409_CONFLICT
+    default_detail = 'The SSO Application already exists.'
+    default_code = 'application_exists'
