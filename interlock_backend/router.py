61588d647e0309ac2fc295165fdbf9787e37bd10
Dylan Blanque <dylan.blanque@gmail.com>
2025-03-07 19:26:50 -0300

    Add custom router for future use.

---
+++ interlock_backend/router.py
@@ -0,0 +1,21 @@
+from rest_framework.routers import DefaultRouter, DynamicRoute
+
+class Router(DefaultRouter):
+	routes = [
+        # Dynamically generated list routes. Generated using
+        # @action(detail=False) decorator on methods of the viewset.
+        DynamicRoute(
+            url=r'^{prefix}/{url_path}{trailing_slash}$',
+            name='{basename}-{url_name}',
+            detail=False,
+            initkwargs={}
+        ),
+        # Dynamically generated detail routes. Generated using
+        # @action(detail=True) decorator on methods of the viewset.
+        DynamicRoute(
+            url=r'^{prefix}/{lookup}/{url_path}{trailing_slash}$',
+            name='{basename}-{url_name}',
+            detail=True,
+            initkwargs={}
+        ),
+    ]
