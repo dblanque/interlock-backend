b55938a9e64c130492387418559bb5b64bc3fb40
Dylan Blanque <dylan.blanque@gmail.com>
2025-03-09 22:59:25 -0300

    Implements retrieve, update, delete, change_status endpoints.

--- core/views/application_group.py
+++ core/views/application_group.py
@@ -22,12 +22,16 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 
 # Exceptions
-from core.exceptions.application_group import ApplicationGroupExists
+from core.exceptions.application_group import (
+	ApplicationGroupExists,
+	ApplicationGroupDoesNotExist
+)
 
 # Others
 from core.exceptions.base import BadRequest
 from core.decorators.login import auth_required
 from django.db import transaction
+from django.forms.models import model_to_dict
 import logging
 ################################################################################
 logger = logging.getLogger(__name__)
@@ -57,7 +61,8 @@ class ApplicationGroupViewSet(BaseViewSet, ApplicationSecurityGroupViewMixin):
 				application=application,
 				ldap_objects=serializer.data["ldap_objects"]
 			)
-			users = list(self.user_queryset.filter(pk__in=serializer.data["users"]))
+			users = list(self.user_queryset.filter(
+				pk__in=serializer.data["users"]))
 			for user in users:
 				asg.users.add(user)
 			asg.save()
@@ -112,16 +117,31 @@ class ApplicationGroupViewSet(BaseViewSet, ApplicationSecurityGroupViewMixin):
 			}
 		)
 
-	@action(detail=True, methods=["get"])
 	@auth_required()
-	def fetch(self, request, pk):
+	def retrieve(self, request, pk):
 		code = 0
 		code_msg = "ok"
-		application_group_id = int(pk)
+		pk = int(pk)
+		if not self.queryset.filter(id=pk).exists():
+			raise ApplicationGroupDoesNotExist
+		application_group = self.queryset.get(id=pk)
+		data_users = []
+		for user in application_group.users.all():
+			data_users.append(user.id)
 		return Response(
 			data={
 				"code": code,
-				"code_msg": code_msg
+				"code_msg": code_msg,
+				"data": {
+					"id": application_group.id,
+					"application": {
+						"id": application_group.application.id,
+						"name": application_group.application.name
+					},
+					"enabled": application_group.enabled,
+					"users": data_users,
+					"ldap_objects": application_group.ldap_objects
+				}
 			}
 		)
 
@@ -130,7 +150,68 @@ class ApplicationGroupViewSet(BaseViewSet, ApplicationSecurityGroupViewMixin):
 		data: dict = request.data
 		code = 0
 		code_msg = "ok"
-		application_group_id = int(pk)
+		pk = int(pk)
+		if not self.queryset.filter(id=pk).exists():
+			raise ApplicationGroupDoesNotExist
+
+		asg = self.queryset.get(id=pk)
+		users = self.user_queryset.filter(
+			pk__in=data["users"]).values_list("id", flat=True)
+		data["users"] = users
+		serializer = self.serializer_class(asg, data=data)
+		if not serializer.is_valid():
+			raise BadRequest(data={
+				"errors": serializer.errors
+			})
+
+		with transaction.atomic():
+			serializer.save()
+
+		return Response(
+			data={
+				"code": code,
+				"code_msg": code_msg
+			}
+		)
+
+	@action(detail=True, methods=["patch"])
+	@auth_required()
+	def change_status(self, request, pk):
+		data: dict = request.data
+		code = 0
+		code_msg = "ok"
+		pk = int(pk)
+		if not self.queryset.filter(id=pk).exists():
+			raise ApplicationGroupDoesNotExist
+		if not "enabled" in data:
+			raise BadRequest(data={
+				"errors": "Missing boolean field 'enabled' in data."
+			})
+		if not isinstance(data["enabled"], bool):
+			raise BadRequest(data={
+				"errors": "Field 'enabled' must be a boolean."
+			})
+		asg = self.queryset.get(id=pk)
+		asg.enabled = data["enabled"]
+		asg.save()
+		return Response(
+			data={
+				"code": code,
+				"code_msg": code_msg
+			}
+		)
+
+	@action(detail=True, methods=["delete"])
+	@auth_required()
+	def delete(self, request, pk):
+		data: dict = request.data
+		code = 0
+		code_msg = "ok"
+		pk = int(pk)
+		if not self.queryset.filter(id=pk).exists():
+			raise ApplicationGroupDoesNotExist
+		asg = self.queryset.get(id=pk)
+		asg.delete_permanently()
 		return Response(
 			data={
 				"code": code,
