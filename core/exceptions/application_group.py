d7df25855a61408ff8e1108cab078eb8a83c512a
Dylan Blanque <dylan.blanque@gmail.com>
2025-03-09 22:59:02 -0300

    Add ApplicationGroupDoesNotExist exception.

--- core/exceptions/application_group.py
+++ core/exceptions/application_group.py
@@ -7,3 +7,8 @@ class ApplicationGroupExists(CoreException):
     status_code = status.HTTP_409_CONFLICT
     default_detail = 'The Application Group already exists.'
     default_code = 'application_group_exists'
+
+class ApplicationGroupDoesNotExist(CoreException):
+    status_code = status.HTTP_404_NOT_FOUND
+    default_detail = 'The Application Group does not exist.'
+    default_code = 'application_group_does_not_exist'
