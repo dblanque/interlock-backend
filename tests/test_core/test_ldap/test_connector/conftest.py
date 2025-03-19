87b85bd8374c7a6c305f1696eb6a4159b161922b
Dylan Blanque <dylan.blanque@gmail.com>
2025-03-19 16:25:30 -0300

    Add m_runtime_settings fixture.

--- tests/test_core/test_ldap/test_connector/conftest.py
+++ tests/test_core/test_ldap/test_connector/conftest.py
@@ -1,7 +1,16 @@
 import pytest
-from unittest.mock import MagicMock
-
+from unittest.mock import MagicMock, Mock
+from core.models.ldap_settings import LDAP_SETTING_MAP
+from core.ldap import defaults as ldap_defaults
 
 @pytest.fixture
 def m_connection(mocker) -> MagicMock:
 	return mocker.MagicMock()
+
+@pytest.fixture
+def m_runtime_settings(mocker) -> MagicMock:
+	mock = mocker.MagicMock()
+	for setting_key, setting_type in LDAP_SETTING_MAP.items():
+		setting_value = getattr(ldap_defaults, setting_key)
+		setattr(mock, setting_key, setting_value)
+	return mock
