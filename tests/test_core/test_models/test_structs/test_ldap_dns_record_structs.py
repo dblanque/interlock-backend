e4c9541784af35769dc63f5bfc267f3cc7c6d120
Dylan Blanque <dylan.blanque@gmail.com>
2025-04-07 15:32:43 -0300

    Add more tests for SOA Record and RPCName Handling.

--- tests/test_core/test_models/test_structs/test_ldap_dns_record_structs.py
+++ tests/test_core/test_models/test_structs/test_ldap_dns_record_structs.py
@@ -1,4 +1,5 @@
 import pytest
+from core.ldap.defaults import LDAP_DOMAIN
 from core.utils.dnstool import new_record
 from core.models.structs.ldap_dns_record import (
     DNS_RECORD,
@@ -10,6 +11,8 @@ from core.models.structs.ldap_dns_record import (
     RecordTypes,
     DNS_RPC_RECORD_TS,
     DNS_RPC_RECORD_NODE_NAME,
+    DNS_RPC_RECORD_STRING,
+    DNS_RPC_NAME,
 )
 from impacket.structure import Structure
 import socket
@@ -72,6 +75,53 @@ def f_dns_record_cname():
     record["Data"] = node
     return DNS_RECORD(record.getData())
 
+@pytest.fixture
+def f_dns_record_txt():
+    record: DNS_RECORD = new_record(
+        RecordTypes.DNS_RECORD_TYPE_TXT.value,
+        serial=1,
+        ttl=180
+    )
+    rpc_name = DNS_RPC_RECORD_STRING()
+    string_data = DNS_RPC_NAME()
+    string_data.toRPCName("mock_text")
+    rpc_name["stringData"] = string_data
+    record["Data"] = rpc_name
+    return DNS_RECORD(record.getData())
+
+@pytest.fixture
+def f_dns_record_soa_values() -> dict:
+    return {
+		"dwSerialNo": 2025040701,
+		"dwRefresh": 900,
+		"dwRetry": 7200,
+		"dwExpire": 86400,
+		"dwMinimumTtl": 900,
+		"namePrimaryServer": f"ldap-server.{LDAP_DOMAIN}.",
+		"zoneAdminEmail": f"hostmaster.{LDAP_DOMAIN}.",
+	}
+
+@pytest.fixture
+def f_dns_record_soa(f_dns_record_soa_values):
+    record: DNS_RECORD = new_record(
+        RecordTypes.DNS_RECORD_TYPE_SOA.value,
+        serial=1,
+        ttl=180
+    )
+    data = DNS_RPC_RECORD_SOA()
+    for f in [
+        "dwSerialNo",
+        "dwRefresh",
+        "dwRetry",
+        "dwExpire",
+        "dwMinimumTtl"
+        ]:
+        data.setField(f, f_dns_record_soa_values[f])
+    for f in ["namePrimaryServer", "zoneAdminEmail"]:
+        data[f] = data.addCountName(f_dns_record_soa_values[f])
+    record["Data"] = data
+    return DNS_RECORD(record.getData())
+
 # Tests -------------------------------------------------------------------- #
 
 class TestDNSRecordStructure:
@@ -82,6 +132,28 @@ class TestDNSRecordStructure:
         assert "DataLength" in structure_fields
         assert "Type" in structure_fields
         assert "Data" in structure_fields
+    
+    def test_dns_record_dunder_bytedata(self, mocker):
+        mocker.patch.object(DNS_RECORD, "getData", return_value = b"record_bytes")
+        record = DNS_RECORD()
+        assert record.__bytedata__() == b"record_bytes"
+    
+    def test_dns_record_dunder_str(self, mocker):
+        m_dict = {"key": "value"}
+        mocker.patch("core.models.structs.ldap_dns_record.record_to_dict", return_value = m_dict)
+        record = DNS_RECORD()
+        assert record.__str__() == str(m_dict)
+    
+    def test_dns_record_dunder_dict(self, mocker):
+        m_dict = {"key": "value"}
+        mocker.patch("core.models.structs.ldap_dns_record.record_to_dict", return_value = m_dict)
+        record = DNS_RECORD()
+        assert record.__dict__() == m_dict
+    
+    def test_dns_record_dunder_getTTL(self, mocker):
+        record = DNS_RECORD()
+        record["TtlSeconds"] = 180
+        assert record.__getTTL__() == 180
 
 class TestARecordHandling:
     def test_a_record_conversion(self, f_dns_record_a):
@@ -121,8 +193,13 @@ class TestCountNameHandling:
     def test_invalid_count_name_length(self):
         count_name = DNS_COUNT_NAME()
         long_string = "a" * 300
-        with pytest.raises(struct.error):
+        with pytest.raises((struct.error, ValueError)):
             count_name.toCountName(long_string)
+
+class TestRPCNameHandling:
+    def test_to_rpc_name_parsing(self, f_dns_record_txt):
+        result = record_to_dict(f_dns_record_txt)
+        assert result["stringData"] == "mock_text"
 
 class TestSOARecordHandling:
     def test_soa_record_serialization(self):
@@ -133,6 +210,12 @@ class TestSOARecordHandling:
         
         assert soa["dwSerialNo"] == 123456
         assert soa["namePrimaryServer"].toFqdn() == "ns1.example.com."
+    
+    def test_soa_record_to_dict(self, f_dns_record_soa, f_dns_record_soa_values: dict):
+        result = record_to_dict(f_dns_record_soa)
+        for k, v in f_dns_record_soa_values.items():
+            assert result[k] == v
+
 
 class TestTSRecordHandling:
     def test_timestamp_conversion(self):
