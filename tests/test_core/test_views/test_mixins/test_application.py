21c4143ad60f27d424d9159ee72ea07467e06f13
Dylan Blanque <dylan.blanque@gmail.com>
2025-04-07 19:09:04 -0300

    Finish tests for Application view mixin.

--- tests/test_core/test_views/test_mixins/test_application.py
+++ tests/test_core/test_views/test_mixins/test_application.py
@@ -1,7 +1,8 @@
 import pytest
 from core.models.application import Application
 from core.views.mixins.application import ApplicationViewMixin
-from oidc_provider.models import Client, ResponseType
+from oidc_provider.models import Client
+from core.ldap.defaults import LDAP_DOMAIN
 from core.exceptions import (
 	base as exc_base,
 	application as exc_app,
@@ -26,6 +27,30 @@ def clean_up():
 	Client.objects.all().delete()
 
 @pytest.fixture
+def f_application_data() -> dict:
+	def maker(**kwargs):
+		mock_dict = {
+			"name":"Mock Application",
+			"redirect_uris": f"https://subdomain.{LDAP_DOMAIN}",
+			"scopes":"scope1 scope2",
+			"enabled": False,
+			"require_consent": False,
+			"reuse_consent": False,
+			"response_types":{
+				'code': False,
+				'id_token': False,
+				'id_token token': False,
+				'code id_token': False,
+				'code id_token token': False,
+				'code token': False,
+			}
+		}
+		for kw_key, kw_val in kwargs.items():
+			mock_dict[kw_key] = kw_val
+		return mock_dict
+	return maker
+
+@pytest.fixture
 def f_pre_application_client() -> tuple[Application, Client]:
 	with transaction.atomic():
 		application = Application.objects.create(
@@ -39,6 +64,7 @@ def f_pre_application_client() -> tuple[Application, Client]:
 			redirect_uris=application.redirect_uris.split(","),
 			scope=application.scopes.split(),
 		)
+		client.response_types.add(1)
 	return application, client
 
 @pytest.fixture
@@ -196,3 +222,77 @@ class TestApplicationMixin:
 			)
 		).sort()
 		assert len(result["applications"]) == 1
+
+	def test_fetch_application(self, f_pre_application: Application, f_pre_client: Client):
+		mixin = ApplicationViewMixin()
+		RESPONSE_TYPE_ID_MAP = mixin.get_response_type_id_map()
+		expected_response_types = {
+			field: False
+			for field in RESPONSE_TYPE_ID_MAP.keys()
+		}
+		expected_response_types["code"] = True
+		data = mixin.fetch_application(application_id=f_pre_application.id)
+		assert isinstance(data, dict)
+		assert data["response_types"] == expected_response_types
+		assert data["client_id"] == f_pre_application.client_id
+		assert data["client_secret"] == f_pre_application.client_secret
+
+	def test_update_application(self, f_pre_application: Application, f_application_data: dict):
+		m_data = f_application_data(**{
+			"name":"New Mock Application",
+			"redirect_uris": f"https://{LDAP_DOMAIN}",
+			"scopes":"scope3 scope4",
+			"enabled": False,
+			"require_consent": False,
+			"reuse_consent": False,
+			"client_id":"changed", # this should not change
+			"client_secret": "changed", # this should not change
+			"response_types":{
+				'code': False,
+				'id_token': False,
+				'id_token token': True,
+				'code id_token': False,
+				'code id_token token': False,
+				'code token': False,
+			}
+		})
+		app, cli = ApplicationViewMixin().update_application(
+			application_id=f_pre_application.id,
+			data=m_data
+		)
+		assert app.name == "New Mock Application"
+		assert app.scopes == "scope3 scope4"
+		assert app.enabled is False
+		assert cli.client_id != "changed"
+		assert cli.client_secret != "changed"
+		assert ["id_token token"] == list(cli.response_types.all().values_list("value", flat=True))
+
+	def test_update_application_invalid_data(self, f_pre_application: Application, f_pre_client: Client):
+		m_data = {
+			"name": False,
+			"some_random_field": b"pepe",
+		}
+		with pytest.raises(exc_base.BadRequest):
+			ApplicationViewMixin().update_application(
+				application_id=f_pre_application.id,
+				data=m_data
+			)
+
+	def test_update_application_client_invalid_data(self, f_pre_application: Application, f_application_data: dict):
+		m_data = f_application_data(**{
+			"require_consent": b"bad_data",
+		})
+		with pytest.raises(exc_base.BadRequest):
+			ApplicationViewMixin().update_application(
+				application_id=f_pre_application.id,
+				data=m_data
+			)
+
+	def test_delete_application_raises_not_exists(self):
+		with pytest.raises(exc_app.ApplicationDoesNotExist):
+			ApplicationViewMixin().delete_application(application_id=0)
+
+	def test_delete_application(self, f_pre_application):
+		ApplicationViewMixin().delete_application(application_id=f_pre_application.id)
+		assert Application.objects.count() == 0
+		assert Client.objects.count() == 0
