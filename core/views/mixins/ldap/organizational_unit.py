a67500fbd48fabd9dbc8460842cb0496899161dc
Dylan Blanque <dylan.blanque@gmail.com>
2025-04-18 19:30:13 -0300

    Re-factor mixin, improve docstrings and functions logic/readability.

--- core/views/mixins/ldap/organizational_unit.py
+++ core/views/mixins/ldap/organizational_unit.py
@@ -11,7 +11,7 @@
 from rest_framework import viewsets
 
 ### Exceptions
-from core.exceptions import dirtree as exc_dirtree
+from core.exceptions import dirtree as exc_dirtree, ldap as exc_ldap
 
 ### Interlock
 from core.ldap.adsi import (
@@ -33,7 +33,9 @@ from core.models.choices.log import (
 
 ### Others
 from django.http.request import HttpRequest
+from rest_framework import status
 from ldap3 import Connection
+from ldap3.utils.dn import safe_dn, safe_rdn
 import logging
 ################################################################################
 
@@ -44,99 +46,144 @@ logger = logging.getLogger(__name__)
 class OrganizationalUnitMixin(viewsets.ViewSetMixin):
 	ldap_connection: Connection
 	request: HttpRequest
-	def process_filter(self, data, filterDict=None):
+	def process_filter(self, data: dict, filter_dict: dict = None):
+		"""Process LDAP Directory Tree Request Filter
+
+		Args:
+			data (dict): Request data dictionary
+			filter_dict (dict, optional): LDAP Filter dictionary to use when
+			iexact is not used. Defaults to None.
+
+		Raises:
+			TypeError: When data is not of type dict.
+
+		Returns:
+			str: LDAP Filter String
+		"""
 		ldap_filter = ""
 
-		if "filter" in data and "iexact" in data["filter"]:
-			logger.debug("Dirtree fetching with Filter iexact")
-			if len(data["filter"]["iexact"]) > 0:
-				for f in data["filter"]["iexact"]:
-					lookup_value = data["filter"]["iexact"][f]
-					if isinstance(lookup_value, dict):
-						lookup_type = lookup_value.pop("attr")
-						if "exclude" in lookup_value:
-							lookup_exclude = lookup_value.pop("exclude")
-						else:
-							lookup_exclude = False
-						if "or" in lookup_value:
-							lookup_or = lookup_value.pop("or")
-						else:
-							lookup_or = False
+		if not isinstance(data, dict):
+			raise TypeError("data must be of type dict.")
+		filter_data: dict = data.get("filter", None)
 
-						if lookup_or:
-							expr = LDAP_FILTER_OR
-						else:
-							expr = LDAP_FILTER_AND
-						ldap_filter = join_ldap_filter(
-							ldap_filter,
-							f"{lookup_type}={f}",
-							expression=expr,
-							negate_add=lookup_exclude,
-						)
+		filter_data_iexact = filter_data.get("iexact", None)
+		if filter_data_iexact and not isinstance(filter_data_iexact, dict):
+			raise TypeError("filter_data_iexact must be of type dict.")
+
+		filter_data_exclude = filter_data.get("exclude", None)
+		if filter_data_exclude and not isinstance(filter_data_exclude, dict):
+			raise TypeError("filter_data_exclude must be of type dict.")
+
+		# Exact Filter
+		if filter_data_iexact:
+			logger.debug("Dirtree fetching with Filter iexact")
+			for f in filter_data_iexact:
+				lookup_value = filter_data_iexact[f]
+				if isinstance(lookup_value, dict):
+					lookup_type = lookup_value.pop("attr")
+					lookup_exclude = False
+					lookup_or = False
+
+					if "exclude" in lookup_value:
+						lookup_exclude = lookup_value.pop("exclude")
+					if "or" in lookup_value:
+						lookup_or = lookup_value.pop("or")
+
+					if lookup_or:
+						expr = LDAP_FILTER_OR
 					else:
-						lookup_type = lookup_value
-						ldap_filter = join_ldap_filter(ldap_filter, f"{lookup_type}={f}")
+						expr = LDAP_FILTER_AND
+					ldap_filter = join_ldap_filter(
+						ldap_filter,
+						f"{lookup_type}={f}",
+						expression=expr,
+						negate_add=lookup_exclude,
+					)
+				else:
+					lookup_type = lookup_value
+					ldap_filter = join_ldap_filter(ldap_filter, f"{lookup_type}={f}")
+		# Standard exclusion filter
 		else:
 			logger.debug("Dirtree fetching with Standard Exclusion Filter")
-			if filterDict is None:
-				filterDict = {
+			if filter_dict is None:
+				filter_dict = {
 					**RuntimeSettings.LDAP_DIRTREE_CN_FILTER,
 					**RuntimeSettings.LDAP_DIRTREE_OU_FILTER,
 				}
-			if "filter" in data and "exclude" in data["filter"]:
-				if len(data["filter"]["exclude"]) > 0:
-					for f in data["filter"]["exclude"]:
-						lookup_type = data["filter"]["exclude"][f]
-						if f in filterDict:
-							del filterDict[f]
+			# Remove excluded field/value pairs from filter dict.
+			if filter_data_exclude:
+				for f in filter_data_exclude:
+					if f in filter_dict:
+						del filter_dict[f]
 
-			ldap_filter = search_filter_from_dict(filterDict)
+			ldap_filter = search_filter_from_dict(filter_dict)
 
-			# Where f is Filter Value, fType is the filter Type (not a Jaguar)
-			# Example: objectClass=computer
-			# f = computer
-			# fType = objectClass
-			if "filter" in data and "exclude" in data["filter"]:
-				if len(data["filter"]["exclude"]) > 0:
-					for f in data["filter"]["exclude"]:
-						lookup_type = data["filter"]["exclude"][f]
-						ldap_filter = join_ldap_filter(
-							ldap_filter, f"{lookup_type}={f}", negate_add=True
-						)
+			# Add excluded field/value pairs as negated filters to dict.
+			if filter_data_exclude:
+				for f in filter_data_exclude:
+					lookup_type = filter_data_exclude[f]
+					ldap_filter = join_ldap_filter(
+						ldap_filter, f"{lookup_type}={f}", negate_add=True
+					)
 
-		logger.debug("LDAP Filter for Dirtree: ")
-		logger.debug(ldap_filter)
-
+		logger.debug("LDAP Filter for Dirtree: %s", ldap_filter)
 		return ldap_filter
 
 	def move_or_rename_object(
 		self, distinguished_name: str, relative_dn: str = None, ldap_path: str = None
 	) -> str:
+		"""Performs Move/Rename on LDAP Entry / Object with specified DN.
+
+		Args:
+			distinguished_name (str): LDAP Entry / Object Distinguished Name.
+			relative_dn (str, optional): Will rename the object if changed from
+				what is in distinguished_name. Defaults to None.
+			ldap_path (str, optional): Will relocate object if changed from what
+				is in distinguished_name. Defaults to None.
+
+		* DN = Distinguished Name
+		* RDN = Relative Distinguished Name		
+		
+		Raises:
+			exc_dirtree.DirtreeDistinguishedNameConflict: Raised when RDN
+				identifier is invalid.
+			exc_dirtree.DirtreeNewNameIsOld: Raised if new RDN is same as the
+				old RDN.
+			exc_dirtree.DirtreeDistinguishedNameConflict: _description_
+			exc_dirtree.DirtreeMove: _description_
+
+		Returns:
+			str: New Distinguished Name for LDAP Entry / Object
 		"""
-		* relative_dn = Will rename the object if changed from what is in distinguished_name
-		* ldap_path = Will relocate object if changed from what is in distinguished_name
-		Returns new object Distinguished Name
-		"""
+		try:
+			distinguished_name = safe_dn(dn=distinguished_name)
+		except Exception as e:
+			logger.exception(e)
+			raise exc_ldap.DistinguishedNameValidationError
+
 		operation = LOG_ACTION_RENAME
 		new_dn = None
 
 		# If relative_dn is passed, namechange will be executed.
 		if relative_dn:
-			new_relative_dn = relative_dn
-			original_relative_dn = distinguished_name.split(",")[0]
-			original_relative_dn_identifier = original_relative_dn.split("=")[0]
+			new_rdn = relative_dn
+			original_rdn = safe_rdn(dn=distinguished_name)[0]
+			original_rdn_field = original_rdn.split("=")[0]
 
 			# Validations
-			if original_relative_dn_identifier.lower() not in RuntimeSettings.LDAP_LDIF_IDENTIFIERS:
-				raise exc_dirtree.DirtreeDistinguishedNameConflict
-			if not new_relative_dn.startswith(original_relative_dn_identifier):
-				new_relative_dn = f"{original_relative_dn_identifier}={new_relative_dn}"
-			if new_relative_dn == original_relative_dn:
+			if original_rdn_field.lower() not in RuntimeSettings.LDAP_LDIF_IDENTIFIERS:
+				raise exc_ldap.LDIFBadField
+
+			if not new_rdn.startswith(original_rdn_field):
+				new_rdn = f"{original_rdn_field}={new_rdn}"
+
+			new_rdn = safe_rdn(dn=new_rdn)[0]
+			if new_rdn == original_rdn:
 				raise exc_dirtree.DirtreeNewNameIsOld
 		else:
-			new_relative_dn = distinguished_name.split(",")[0]
+			new_rdn = safe_rdn(dn=distinguished_name)[0]
 
-		if new_relative_dn == distinguished_name:
+		if new_rdn == distinguished_name:
 			raise exc_dirtree.DirtreeDistinguishedNameConflict
 
 		if ldap_path:
@@ -144,32 +191,37 @@ class OrganizationalUnitMixin(viewsets.ViewSetMixin):
 		try:
 			if ldap_path:
 				self.ldap_connection.modify_dn(
-					distinguished_name, new_relative_dn, new_superior=ldap_path
+					dn=distinguished_name,
+					relative_dn=new_rdn,
+					new_superior=ldap_path
 				)
-				new_dn = f"{new_relative_dn},{ldap_path}"
+				new_dn = f"{new_rdn},{ldap_path}"
 			else:
-				self.ldap_connection.modify_dn(distinguished_name, new_relative_dn)
+				self.ldap_connection.modify_dn(distinguished_name, new_rdn)
 				new_path = distinguished_name.split(",")
 				del new_path[0]
 				new_path = ",".join(new_path)
-				new_dn = f"{new_relative_dn},{new_path}"
+				new_dn = f"{new_rdn},{new_path}"
 		except Exception as e:
 			logger.exception(e)
-			data = {
+			_code = None
+			result_description: str = getattr(
+				self.ldap_connection.result, "description", None)
+			if result_description and isinstance(result_description, str):
+				if result_description == "entryAlreadyExists":
+					_code = 409
+			raise exc_dirtree.DirtreeMove(data={
 				"ldap_response": self.ldap_connection.result,
-				"ldapObject": new_relative_dn,
-			}
-			if hasattr(self.ldap_connection.result, "description"):
-				if self.ldap_connection.result.description == "entryAlreadyExists":
-					data["code"] = 409
-			self.ldap_connection.unbind()
-			raise exc_dirtree.DirtreeMove(data=data)
+				"ldapObject": new_rdn,
+				"code": status.HTTP_500_INTERNAL_SERVER_ERROR if not _code \
+						else _code
+			})
 
 		DBLogMixin.log(
 			user=self.request.user.id,
 			operation_type=LOG_ACTION_UPDATE,
 			log_target_class=LOG_CLASS_LDAP,
-			log_target=new_relative_dn,
+			log_target=new_rdn,
 			message=operation,
 		)
 		return new_dn
