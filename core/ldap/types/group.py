e17e5aa575fc5821c98bb69ac73b25723c9f2c74
Dylan Blanque <dylan.blanque@gmail.com>
2025-04-21 18:24:06 -0300

    Update types to Enum. Dict kept for compatibility.

--- core/ldap/types/group.py
+++ core/ldap/types/group.py
@@ -3,13 +3,17 @@
 ################## ORIGINAL PROJECT CREATED BY DYLAN BLANQUÉ ###################
 ########################## AND BR CONSULTING S.R.L. ############################
 ################################################################################
-# Module: interlock_backend.ldap.groupTypes
+# Module: core.ldap.types.group
+from enum import Enum
+
+class LDAPGroupTypes(Enum):
+	GROUP_DISTRIBUTION = 0
+	GROUP_SYSTEM = 1
+	GROUP_GLOBAL = 2
+	GROUP_DOMAIN_LOCAL = 4
+	GROUP_UNIVERSAL = 8
+	GROUP_SECURITY = 2147483648
 
 LDAP_GROUP_TYPES = {
-	"GROUP_DISTRIBUTION": 0,
-	"GROUP_SYSTEM": 1,
-	"GROUP_GLOBAL": 2,
-	"GROUP_DOMAIN_LOCAL": 4,
-	"GROUP_UNIVERSAL": 8,
-	"GROUP_SECURITY": 2147483648,
+	t.name: t.value for t in LDAPGroupTypes
 }
