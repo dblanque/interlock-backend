7c0c79604dd289d64abd130269d83b198262907c
Dylan Blanque <dylan.blanque@gmail.com>
2025-04-25 16:11:04 -0300

    Add disabled account test.

---
+++ tests/test_core/test_views/test_account_disabled.py
@@ -0,0 +1,17 @@
+import pytest
+from rest_framework.test import APIClient
+from rest_framework.response import Response
+from rest_framework import status
+
+@pytest.mark.parametrize(
+	"url, method",
+	(
+		("/api/ldap/record/insert/", "post"),
+		("/api/ldap/record/update/", "put"),
+		("/api/ldap/record/delete/", "post"),
+	),
+)
+def test_disabled_user_unauthorized(url: str, method: str, disabled_user_client: APIClient):
+	method = getattr(disabled_user_client, method)
+	response: Response = method(url)
+	assert response.status_code == status.HTTP_401_UNAUTHORIZED
