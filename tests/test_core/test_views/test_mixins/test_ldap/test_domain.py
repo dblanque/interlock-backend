0d8d8ca9ccdc10047b0ae837501c8eb6fc836cfe
Dylan Blanque <dylan.blanque@gmail.com>
2025-04-27 23:39:18 -0300

    Add test for test_create_initial_serial

--- tests/test_core/test_views/test_mixins/test_ldap/test_domain.py
+++ tests/test_core/test_views/test_mixins/test_ldap/test_domain.py
@@ -1,8 +1,9 @@
 import pytest
 from core.views.mixins.ldap.domain import DomainViewMixin
-from core.models.dns import LDAPRecord
+from core.models.dns import LDAPRecord, DATE_FMT
 from core.ldap.defaults import LDAP_DOMAIN
 from unittest.mock import MagicMock
+from datetime import datetime
 
 
 @pytest.fixture
@@ -19,8 +20,8 @@ def f_mock_soa_record():
 	return mock
 
 
-class TestDomainViewMixin:
-	def test_get_zone_soa(self, f_domain_mixin, f_mock_soa_record, mocker):
+class TestDomainViewMixinUtils:
+	def test_get_zone_soa(self, f_domain_mixin: DomainViewMixin, f_mock_soa_record: LDAPRecord, mocker):
 		mocker.patch(
 			"core.views.mixins.ldap.domain.LDAPRecord",
 			return_value=f_mock_soa_record,
@@ -29,6 +30,19 @@ class TestDomainViewMixin:
 		result = f_domain_mixin.get_zone_soa(LDAP_DOMAIN)
 		assert result == f_mock_soa_record.data
 
-	def test_increment_soa_serial(self, f_domain_mixin, f_mock_soa_record):
+	def test_increment_soa_serial(self, f_domain_mixin: DomainViewMixin, f_mock_soa_record: LDAPRecord):
 		result = f_domain_mixin.increment_soa_serial(f_mock_soa_record, 101)
 		f_mock_soa_record.update.assert_called_once()
+
+	@staticmethod
+	@pytest.mark.parametrize(
+		"as_epoch, expected",
+		(
+			(True, int(datetime.today().strftime(DATE_FMT) + "01")),
+			(False, 1),
+		),
+	)
+	def test_create_initial_serial(as_epoch: bool, expected: int, f_domain_mixin: DomainViewMixin):
+		assert f_domain_mixin.create_initial_serial(
+			as_epoch_serial=as_epoch
+		) == expected
