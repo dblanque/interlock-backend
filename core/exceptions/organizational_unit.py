772c765633dfcfaf602d790b1a8c161f797951e2
Dylan Blanque <dylan.blanque@gmail.com>
2025-04-29 17:40:25 -0300

    Change MissingField OU Exception status code.

--- core/exceptions/organizational_unit.py
+++ core/exceptions/organizational_unit.py
@@ -11,6 +11,6 @@ class OUCreate(CoreException):
 
 
 class MissingField(CoreException):
-	status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+	status_code = status.HTTP_400_BAD_REQUEST
 	default_detail = "A field for the Organizational Unit is missing"
 	default_code = "ou_field_error"
