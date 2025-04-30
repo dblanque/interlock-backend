2960d7c9befecbf17c68a83315bd2613ee7569b5
Dylan Blanque <dylan.blanque@gmail.com>
2025-04-30 20:14:00 -0300

    Add Group Serializer (to be implemented).

---
+++ core/serializers/group.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from core.serializers.ldap import DistinguishedNameField
+from django.core.validators import validate_email
+
+class LDAPGroupSerializer(serializers.Serializer):
+	# Common Name
+	cn = serializers.CharField(required=False)
+	# Distinguished Name
+	distinguishedName = DistinguishedNameField
+	# Mail
+	mail = serializers.CharField(
+		allow_blank=True,
+		required=False,
+		validators=[validate_email],
+	)
+	email = serializers.CharField(
+		allow_blank=True,
+		required=False,
+		validators=[validate_email],
+	)
+	groupType = serializers.IntegerField()
+	groupScopes = serializers.IntegerField()
+	member = serializers.ListField()
