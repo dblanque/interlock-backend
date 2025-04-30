71a9088709f0a0b239ae3d8cdb4d86c0f530e9b6
Dylan Blanque <dylan.blanque@gmail.com>
2025-04-30 20:13:51 -0300

    Move validators to separate ldap file.

---
+++ core/serializers/ldap.py
@@ -0,0 +1,55 @@
+from rest_framework import serializers
+from rest_framework.serializers import ValidationError
+import re
+from ldap3.utils.dn import parse_dn
+from core.ldap.adsi import LDAP_PERMS
+from core.ldap.countries import LDAP_COUNTRIES
+
+def ldap_user_validator(v: str):
+	def has_invalid_chars(s: str):
+		return re.match(r'.*[\.\@\]\["\:\;\|\=\+\*\?\<\>\/\\\,]', s) is not None
+	return not has_invalid_chars(v)
+
+def ldap_user_validator_se(v: str):
+	if not ldap_user_validator(v):
+		raise ValidationError("Username contains invalid characters.")
+	return v
+
+def dn_validator_se(v: str):
+	try:
+		parse_dn(v)
+	except:
+		raise ValidationError("Could not parse Distinguished Name.")
+	return v
+
+def country_validator(v: str):
+	if not v.strip().lower() in [c.lower() for c in LDAP_COUNTRIES.keys()]:
+		raise ValidationError("Invalid country name.")
+	return v
+
+def country_dcc_validator(v: int):
+	try:
+		for codes in LDAP_COUNTRIES.values():
+			if int(codes.get("dccCode")) == v:
+				return v
+	except:
+		pass
+	raise ValidationError(f"Country DCC code ({v}) not found.")
+
+def country_iso_validator(v: str):
+	if len(v) > 2:
+		raise ValidationError("Country ISO code cannot be longer than 2 chars.")
+	try:
+		for codes in LDAP_COUNTRIES.values():
+			if codes.get("isoCode") == v:
+				return v
+	except:
+		pass
+	raise ValidationError(f"Country DCC code ({v}) not found.")
+
+def ldap_permission_validator(v: str):
+	if not v in LDAP_PERMS.keys():
+		raise ValidationError(f"LDAP Permission is invalid ({v}).")
+	return v
+
+DistinguishedNameField = serializers.CharField(required=False, validators=[dn_validator_se])
--- core/serializers/user.py
+++ core/serializers/user.py
@@ -20,55 +20,15 @@ from core.ldap.constants import (
 	LDAP_ATTR_STATE,
 	LDAP_ATTR_COUNTRY,
 )
-from core.ldap.countries import LDAP_COUNTRIES
-from core.ldap.adsi import LDAP_PERMS
-
-def ldap_user_validator(v: str):
-	def has_invalid_chars(s: str):
-		return re.match(r'.*[\.\@\]\["\:\;\|\=\+\*\?\<\>\/\\\,]', s) is not None
-	return not has_invalid_chars(v)
-
-def ldap_user_validator_se(v: str):
-	if not ldap_user_validator(v):
-		raise ValidationError("Username contains invalid characters.")
-	return v
-
-def dn_validator_se(v: str):
-	try:
-		parse_dn(v)
-	except:
-		raise ValidationError("Could not parse Distinguished Name.")
-	return v
-
-def country_validator(v: str):
-	if not v.strip().lower() in [c.lower() for c in LDAP_COUNTRIES.keys()]:
-		raise ValidationError("Invalid country name.")
-	return v
-
-def country_dcc_validator(v: int):
-	try:
-		for codes in LDAP_COUNTRIES.values():
-			if int(codes.get("dccCode")) == v:
-				return v
-	except:
-		pass
-	raise ValidationError(f"Country DCC code ({v}) not found.")
-
-def country_iso_validator(v: str):
-	if len(v) > 2:
-		raise ValidationError("Country ISO code cannot be longer than 2 chars.")
-	try:
-		for codes in LDAP_COUNTRIES.values():
-			if codes.get("isoCode") == v:
-				return v
-	except:
-		pass
-	raise ValidationError(f"Country DCC code ({v}) not found.")
-
-def ldap_permission_validator(v: str):
-	if not v in LDAP_PERMS.keys():
-		raise ValidationError(f"LDAP Permission is invalid ({v}).")
-	return v
+from core.serializers.ldap import (
+	ldap_user_validator,
+	DistinguishedNameField,
+	ldap_user_validator_se,
+	country_validator,
+	country_iso_validator,
+	country_dcc_validator,
+	ldap_permission_validator,
+)
 
 FIELD_VALIDATORS = {
 	LOCAL_ATTR_USERNAME: ldap_user_validator,  # username
@@ -131,7 +91,7 @@ class LDAPUserSerializer(serializers.Serializer):
 	password = serializers.CharField(required=False)
 
 	# Distinguished Name
-	distinguishedName = serializers.CharField(required=False, validators=[dn_validator_se])
+	distinguishedName = DistinguishedNameField
 	type = serializers.CharField(required=False)
 	# First Name
 	givenName = serializers.CharField(
@@ -152,7 +112,11 @@ class LDAPUserSerializer(serializers.Serializer):
 		required=False,
 		validators=[validate_email]
 	)
-	email = serializers.CharField(required=False, validators=[validate_email])
+	email = serializers.CharField(
+		allow_blank=True,
+		required=False,
+		validators=[validate_email],
+	)
 	# Postal Code
 	postalCode = serializers.CharField(required=False)
 	# City
