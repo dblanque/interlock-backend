baf344616ff17f53770ac1ad08ed3614404a05bb
Dylan Blanque <dylan.blanque@gmail.com>
2025-05-07 17:15:39 -0300

    Use constants instead of hardcoded values.

--- core/constants/group.py
+++ core/constants/group.py
@@ -1,26 +1,32 @@
 from core.models.ldap_settings_runtime import RuntimeSettingsSingleton
-
+from core.ldap.constants import *
 
 class GroupViewsetFilterAttributeBuilder:
 	def __init__(self, settings: RuntimeSettingsSingleton):
 		self.RunningSettings = settings
 
 	def get_list_filter(self):
-		return ["cn", "distinguishedName", "groupType", "member"]
+		return [
+			LDAP_ATTR_COMMON_NAME,
+			LDAP_ATTR_DN,
+			LDAP_ATTR_GROUP_TYPE,
+			LDAP_ATTR_GROUP_MEMBERS,
+		]
 
 	def get_fetch_filter(self):
 		return [
-			"cn",
-			"mail",
-			"member",
-			"distinguishedName",
-			"groupType",
-			"objectSid",
+			LDAP_ATTR_COMMON_NAME,
+			LDAP_ATTR_EMAIL,
+			LDAP_ATTR_GROUP_MEMBERS,
+			LDAP_ATTR_DN,
+			LDAP_ATTR_GROUP_TYPE,
+			LDAP_ATTR_SECURITY_ID,
+			LDAP_ATTR_OBJECT_CLASS,
 		]
 
 	def get_insert_filter(self):
 		return [
-			"cn",
-			"distinguishedName",
-			"userPrincipalName",
+			LDAP_ATTR_COMMON_NAME,
+			LDAP_ATTR_DN,
+			LDAP_ATTR_UPN,
 		]
