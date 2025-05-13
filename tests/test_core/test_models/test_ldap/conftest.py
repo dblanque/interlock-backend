df115de1cb1f4f8a233500d886f3f2e32ebd2bbe
Dylan Blanque <dylan.blanque@gmail.com>
2025-05-13 20:38:24 -0300

    Remove old tests that should be re-done. Move some fixtures to folder conftest.

---
+++ tests/test_core/test_models/test_ldap/conftest.py
@@ -0,0 +1,145 @@
+########################### Standard Pytest Imports ############################
+import pytest
+from pytest_mock import MockerFixture
+################################################################################
+from datetime import datetime
+from core.views.mixins.utils import is_non_str_iterable
+from core.constants.attrs import *
+from core.models.ldap_settings_runtime import RuntimeSettingsSingleton
+from ldap3 import (
+	Attribute as LDAPAttribute,
+	Entry as LDAPEntry
+)
+
+@pytest.fixture
+def f_object_attrs_user(f_runtime_settings: RuntimeSettingsSingleton) -> dict:
+	def maker():
+		fake_date = datetime.today().strftime(LDAP_DATE_FORMAT)
+		return {
+			LDAP_ATTR_DN: f"CN=Test User,OU=Administrators,{f_runtime_settings.LDAP_AUTH_SEARCH_BASE}",
+			LDAP_ATTR_FIRST_NAME: "Test",
+			LDAP_ATTR_LAST_NAME: "User",
+			LDAP_ATTR_FULL_NAME: "Test User",
+			LDAP_ATTR_USERNAME_SAMBA_ADDS: "testuser",
+			LDAP_ATTR_EMAIL: f"testuser@{f_runtime_settings.LDAP_DOMAIN}",
+			LDAP_ATTR_PHONE: "+5491112345678",
+			LDAP_ATTR_ADDRESS: "Street Address Example",
+			LDAP_ATTR_POSTAL_CODE: "POSTALCODE",
+			LDAP_ATTR_CITY: "Some Town",
+			LDAP_ATTR_STATE: "Buenos Aires",
+			LDAP_ATTR_COUNTRY_DCC: 32,
+			LDAP_ATTR_COUNTRY: "Argentina",
+			LDAP_ATTR_COUNTRY_ISO: "AR",
+			LDAP_ATTR_WEBSITE: f"https://{f_runtime_settings.LDAP_DOMAIN}",
+			LDAP_ATTR_UPN: f"testuser@{f_runtime_settings.LDAP_AUTH_ACTIVE_DIRECTORY_DOMAIN}",
+			LDAP_ATTR_UAC: 66048,
+			LDAP_ATTR_PRIMARY_GROUP_ID: 513,
+			LDAP_ATTR_CREATED: fake_date,
+			LDAP_ATTR_MODIFIED: fake_date,
+			LDAP_ATTR_OBJECT_CLASS: ["top", "person", "organizationalPerson", "user"],
+			LDAP_ATTR_OBJECT_CATEGORY: f"CN=Person,CN=Schema,CN=Configuration,{f_runtime_settings.LDAP_AUTH_SEARCH_BASE}",
+			# Expected SID: "S-1-5-21-123456789-1234567890-123456789-1105"
+			LDAP_ATTR_SECURITY_ID: b"\x01\x05\x00\x00\x00\x00\x00\x05\x15\x00\x00\x00\x11^\xb3\x83j\x06\x94\x00\x80\xdbi\xaaQ\x04\x00\x00",
+			LDAP_ATTR_RELATIVE_ID: 1105,
+			LDAP_ATTR_LAST_LOGIN: "fake_logon_date",
+			LDAP_ATTR_BAD_PWD_COUNT: 0,
+			LDAP_ATTR_PWD_SET_AT: "fake_pwd_last_set",
+			LDAP_ATTR_ACCOUNT_TYPE: 805306368,
+			LDAP_ATTR_USER_GROUPS: f"CN=Administrators,CN=Builtin,{f_runtime_settings.LDAP_AUTH_SEARCH_BASE}",
+		}
+	return maker
+
+
+@pytest.fixture
+def f_object_attrs_group(f_runtime_settings: RuntimeSettingsSingleton) -> dict:
+	def maker():
+		return {
+			LDAP_ATTR_DN: f"CN=Test Group,{f_runtime_settings.LDAP_AUTH_SEARCH_BASE}",
+			LDAP_ATTR_SECURITY_ID: b"\x01\x05\x00\x00\x00\x00\x00\x05\x15\x00\x00\x00\x11^\xb3\x83j\x06\x94\x00\x80\xdbi\xaaR\x04\x00\x00",
+			LDAP_ATTR_RELATIVE_ID: 1106,
+			LDAP_ATTR_OBJECT_CATEGORY: f"CN=Group,CN=Schema,CN=Configuration,{f_runtime_settings.LDAP_AUTH_SEARCH_BASE}",
+			LDAP_ATTR_OBJECT_CLASS: ["top", "group"],
+		}
+
+	return maker
+
+class FakeLDAPUserEntry:
+	def __new__(cls):
+		for attr in (
+			LDAP_ATTR_DN,
+			LDAP_ATTR_FIRST_NAME,
+			LDAP_ATTR_LAST_NAME,
+			LDAP_ATTR_FULL_NAME,
+			LDAP_ATTR_USERNAME_SAMBA_ADDS,
+			LDAP_ATTR_EMAIL,
+			LDAP_ATTR_PHONE,
+			LDAP_ATTR_ADDRESS,
+			LDAP_ATTR_POSTAL_CODE,
+			LDAP_ATTR_CITY,
+			LDAP_ATTR_STATE,
+			LDAP_ATTR_COUNTRY_DCC,
+			LDAP_ATTR_COUNTRY,
+			LDAP_ATTR_COUNTRY_ISO,
+			LDAP_ATTR_WEBSITE,
+			LDAP_ATTR_UPN,
+			LDAP_ATTR_UAC,
+			LDAP_ATTR_PRIMARY_GROUP_ID,
+			LDAP_ATTR_CREATED,
+			LDAP_ATTR_MODIFIED,
+			LDAP_ATTR_OBJECT_CLASS,
+			LDAP_ATTR_OBJECT_CATEGORY,
+			LDAP_ATTR_SECURITY_ID,
+			LDAP_ATTR_RELATIVE_ID,
+			LDAP_ATTR_LAST_LOGIN,
+			LDAP_ATTR_BAD_PWD_COUNT,
+			LDAP_ATTR_PWD_SET_AT,
+			LDAP_ATTR_ACCOUNT_TYPE,
+			LDAP_ATTR_USER_GROUPS,
+		):
+			setattr(cls, attr, None)
+
+@pytest.fixture
+def f_object_entry_user(f_object_attrs_user, mocker: MockerFixture):
+	def maker(attrs=None, **kwargs):
+		if not attrs:
+			attrs = {}
+		m_attrs: dict = f_object_attrs_user() | attrs
+		m_entry = mocker.Mock(spec=FakeLDAPUserEntry)
+		for attr, val in m_attrs.items():
+			m_attr = mocker.Mock(spec=LDAPAttribute)
+			m_attr.value = val
+			if is_non_str_iterable(val):
+				m_attr.values = val
+			elif isinstance(val, (bytes, bytearray)):
+				m_attr.raw_values = [val]
+			else:
+				m_attr.values = [val]
+				m_attr.value = val
+			setattr(m_entry, attr, m_attr)
+		m_entry.entry_attributes = [*set(m_attrs.keys())]
+		m_entry.entry_dn = m_attrs[LDAP_ATTR_DN]
+		return m_entry
+
+	return maker
+
+
+@pytest.fixture
+def f_object_entry_group(f_object_attrs_group, mocker: MockerFixture):
+	def maker(attrs=None, **kwargs):
+		if not attrs:
+			attrs = {}
+		m_attrs: dict = f_object_attrs_group() | attrs
+		m_entry = mocker.Mock()
+		for attr, val in m_attrs.items():
+			m_attr = mocker.Mock(spec=LDAPAttribute)
+			m_attr.values = val
+			if is_non_str_iterable(val):
+				m_attr.value = val if len(val) > 1 else val[0]
+			else:
+				m_attr.value = val
+			setattr(m_entry, attr, m_attr)
+		m_entry.entry_attributes = [*set(m_attrs.keys())]
+		m_entry.entry_dn = m_attrs["distinguishedName"]
+		return m_entry
+
+	return maker
--- tests/test_core/test_models/test_ldap/test_ldap_object.py
+++ tests/test_core/test_models/test_ldap/test_ldap_object.py
@@ -2,28 +2,12 @@
 import pytest
 from pytest import FixtureRequest
 from pytest_mock import MockerFixture, MockType
-
 ################################################################################
 from core.ldap.security_identifier import SID
-from core.models.ldap_object import (
-	LDAPObject,
-	DEFAULT_REQUIRED_LDAP_ATTRS,
-)
-from core.constants.attrs import (
-	LOCAL_ATTR_DN,
-	LDAP_ATTR_DN,
-	LDAP_ATTR_FIRST_NAME,
-	LOCAL_ATTR_FIRST_NAME,
-	LDAP_ATTR_LAST_NAME,
-	LOCAL_ATTR_LAST_NAME,
-	LDAP_ATTR_USER_GROUPS,
-	LDAP_ATTR_PRIMARY_GROUP_ID,
-)
+from core.models.ldap_object import LDAPObject
+from core.constants.attrs import *
 from core.ldap.filter import LDAPFilter
-from core.views.mixins.utils import is_non_str_iterable
-from copy import deepcopy
 from ldap3 import (
-	Attribute as LDAPAttribute,
 	Entry as LDAPEntry,
 	ALL_OPERATIONAL_ATTRIBUTES,
 	ALL_ATTRIBUTES,
@@ -48,147 +32,6 @@ def f_object_args(f_connection, f_runtime_settings: RuntimeSettingsSingleton):
 			"distinguished_name": f"cn=testobject,{f_runtime_settings.LDAP_AUTH_SEARCH_BASE}",
 			**kwargs,
 		}
-
-	return maker
-
-
-@pytest.fixture
-def f_object_attrs_user(f_runtime_settings: RuntimeSettingsSingleton) -> dict:
-	def maker():
-		return {
-			"name": "Test User",
-			"distinguishedName": f"CN=Test User,OU=Administrators,{f_runtime_settings.LDAP_AUTH_SEARCH_BASE}",
-			"type": "Person",
-			"givenName": "Test",
-			"sn": "User",
-			"displayName": "Test User",
-			"sAMAccountName": "testuser",
-			"username": "testuser",
-			"mail": f"testuser@{f_runtime_settings.LDAP_DOMAIN}",
-			"telephoneNumber": "+5491112345678",
-			"streetAddress": "Street Address Example",
-			"postalCode": "POSTALCODE",
-			"l": "Some Town",
-			"st": "Buenos Aires",
-			"countryCode": 32,
-			"co": "Argentina",
-			"c": "AR",
-			"wWWHomePage": f"https://{f_runtime_settings.LDAP_DOMAIN}",
-			"userPrincipalName": f"testuser@{f_runtime_settings.LDAP_AUTH_ACTIVE_DIRECTORY_DOMAIN}",
-			"userAccountControl": 66048,
-			"primaryGroupID": 513,
-			"whenCreated": "fake_creation_date",
-			"whenChanged": "fake_changed_date",
-			"objectClass": ["top", "person", "organizationalPerson", "user"],
-			"objectCategory": f"CN=Person,CN=Schema,CN=Configuration,{f_runtime_settings.LDAP_AUTH_SEARCH_BASE}",
-			"objectSid": b"\x01\x05\x00\x00\x00\x00\x00\x05\x15\x00\x00\x00\x11^\xb3\x83j\x06\x94\x00\x80\xdbi\xaaQ\x04\x00\x00",
-			"objectRid": 1105,
-			"lastLogon": "fake_logon_date",
-			"badPwdCount": 0,
-			"pwdLastSet": "fake_pwd_last_set",
-			"sAMAccountType": 805306368,
-			"memberOf": f"CN=Administrators,CN=Builtin,{f_runtime_settings.LDAP_AUTH_SEARCH_BASE}",
-		}
-
-	return maker
-
-
-@pytest.fixture
-def f_object_attrs_group(f_runtime_settings: RuntimeSettingsSingleton) -> dict:
-	def maker():
-		return {
-			"name": "Test Group",
-			"distinguishedName": f"cn=Test Group,{f_runtime_settings.LDAP_AUTH_SEARCH_BASE}",
-			"type": "Group",
-			"objectSid": b"\x01\x05\x00\x00\x00\x00\x00\x05\x15\x00\x00\x00\x11^\xb3\x83j\x06\x94\x00\x80\xdbi\xaaR\x04\x00\x00",
-			"objectRid": 1106,
-			"objectCategory": f"CN=Group,CN=Schema,CN=Configuration,{f_runtime_settings.LDAP_AUTH_SEARCH_BASE}",
-			"objectClass": ["top", "group"],
-		}
-
-	return maker
-
-
-class FakeUserEntry:
-	entry_dn: str = None
-	entry_attributes: str = None
-	name: str = None
-	distinguishedName: str = None
-	type: str = None
-	givenName: str = None
-	sn: str = None
-	displayName: str = None
-	sAMAccountName: str = None
-	username: str = None
-	mail: str = None
-	telephoneNumber: str = None
-	streetAddress: str = None
-	postalCode: str = None
-	l: str = None
-	st: str = None
-	countryCode: str = None
-	co: str = None
-	c: str = None
-	wWWHomePage: str = None
-	userPrincipalName: str = None
-	userAccountControl: str = None
-	primaryGroupID: str = None
-	whenCreated: str = None
-	whenChanged: str = None
-	objectClass: list[str] = None
-	objectCategory: str = None
-	objectSid: bytes = None
-	objectRid: str = None
-	lastLogon: str = None
-	sAMAccountType: str = None
-	memberOf: list[str] = None
-	pwdLastSet: int = None
-	badPwdCount: int = None
-
-
-@pytest.fixture
-def f_object_entry_user(f_object_attrs_user, mocker: MockerFixture):
-	def maker(attrs=None, **kwargs):
-		if not attrs:
-			attrs = {}
-		m_attrs: dict = f_object_attrs_user() | attrs
-		m_entry = mocker.Mock(spec=FakeUserEntry)
-		for attr, val in m_attrs.items():
-			m_attr = mocker.Mock(spec=LDAPAttribute)
-			m_attr.value = val
-			if is_non_str_iterable(val):
-				m_attr.values = val
-			elif isinstance(val, (bytes, bytearray)):
-				m_attr.raw_values = [val]
-			else:
-				m_attr.values = [val]
-				m_attr.value = val
-			setattr(m_entry, attr, m_attr)
-		m_entry.entry_attributes = [*set(m_attrs.keys())]
-		m_entry.entry_dn = m_attrs["distinguishedName"]
-		return m_entry
-
-	return maker
-
-
-@pytest.fixture
-def f_object_entry_group(f_object_attrs_group, mocker: MockerFixture):
-	def maker(attrs=None, **kwargs):
-		if not attrs:
-			attrs = {}
-		m_attrs: dict = f_object_attrs_group() | attrs
-		m_entry = mocker.Mock()
-		for attr, val in m_attrs.items():
-			m_attr = mocker.Mock(spec=LDAPAttribute)
-			m_attr.values = val
-			if is_non_str_iterable(val):
-				m_attr.value = val if len(val) > 1 else val[0]
-			else:
-				m_attr.value = val
-			setattr(m_entry, attr, m_attr)
-		m_entry.entry_attributes = [*set(m_attrs.keys())]
-		m_entry.entry_dn = m_attrs["distinguishedName"]
-		return m_entry
 
 	return maker
 
@@ -368,7 +211,7 @@ def test_ddr_validate_init_success(
 		b"bytes",
 	)
 )
-def test_d_set_search_attrs_raises_type_error(mocker: MockerFixture, bad_value):
+def test_ddr_set_search_attrs_raises_type_error(mocker: MockerFixture, bad_value):
 	mocker.patch.object(LDAPObject, "__init__", return_value=None)
 	m_ldap_object = LDAPObject()
 	with pytest.raises(TypeError):
@@ -393,12 +236,26 @@ def test_d_set_search_attrs_raises_type_error(mocker: MockerFixture, bad_value):
 		),
 	),
 )
-def test_d_set_search_attrs(mocker: MockerFixture, search_attrs, expected):
+def test_ddr_set_search_attrs(mocker: MockerFixture, search_attrs, expected):
 	mocker.patch.object(LDAPObject, "__init__", return_value=None)
 	m_ldap_object = LDAPObject()
 	m_ldap_object.excluded_attributes = (LDAP_ATTR_FIRST_NAME,)
 	m_ldap_object.__set_search_attrs__(search_attrs)
 	assert m_ldap_object.search_attrs == expected
+
+def test_ddr_set_search_attrs_is_falsy(mocker: MockerFixture):
+	mocker.patch.object(LDAPObject, "__init__", return_value=None)
+	m_ldap_object = LDAPObject()
+	assert m_ldap_object.__set_search_attrs__(None) is None
+	assert m_ldap_object.search_attrs == ALL_OPERATIONAL_ATTRIBUTES
+
+def test_ddr_set_kwargs(mocker: MockerFixture):
+	mocker.patch.object(LDAPObject, "__init__", return_value=None)
+	m_set_search_attrs = mocker.patch.object(LDAPObject, "__set_search_attrs__")
+	m_ldap_object = LDAPObject()
+	m_ldap_object.__set_kwargs__(test=True)
+	assert m_ldap_object.test is True
+	m_set_search_attrs.assert_called_once_with(ALL_OPERATIONAL_ATTRIBUTES)
 
 @pytest.mark.parametrize(
 	"get_name_args",
@@ -422,7 +279,7 @@ def test_d_set_search_attrs(mocker: MockerFixture, search_attrs, expected):
 		"__get_object__ returns attributes",
 	],
 )
-def test_ddr_get__methods(mocker, get_name_args):
+def test_ddr_get_methods(mocker, get_name_args):
 	cls_attribute = get_name_args[1] or get_name_args[0]
 	cls_method = get_name_args[0]
 	mocker.patch.object(LDAPObject, "__init__", return_value=None)
@@ -431,202 +288,46 @@ def test_ddr_get__methods(mocker, get_name_args):
 	method = getattr(m_ldap_object, f"__get_{cls_method}__")
 	assert method() == cls_attribute
 
-
-def test_ddr_fetch_object_returns_none_on_empty_response(
-	f_connection, f_object_args
-):
-	object_args = f_object_args()
-	m_ldap_object = LDAPObject(**object_args)
-	f_connection.entries = []
-	f_connection.search.assert_called_once_with(
-		search_base=m_ldap_object.search_base,
-		search_filter=m_ldap_object.search_filter,
-		search_scope="SUBTREE",
-		attributes=m_ldap_object.search_attrs,
+def test_ddr_sync_int_fields(mocker: MockerFixture):
+	mocker.patch.object(LDAPObject, "__init__", return_value=None)
+	fields = (
+		LOCAL_ATTR_COUNTRY_DCC,
+		LOCAL_ATTR_UAC,
+		LOCAL_ATTR_LAST_LOGIN_WIN32,
+		LOCAL_ATTR_BAD_PWD_COUNT,
+		LOCAL_ATTR_PWD_SET_AT,
+		LOCAL_ATTR_PRIMARY_GROUP_ID,
+		LOCAL_ATTR_RELATIVE_ID,
+		LOCAL_ATTR_ACCOUNT_TYPE,
 	)
-	assert m_ldap_object.__fetch_object__() is None
+	m_ldap_object = LDAPObject()
+	m_ldap_object.attributes = { _key: str(1234) for _key in fields }
+	m_ldap_object.__sync_int_fields__()
+	for fld in fields:
+		assert m_ldap_object.attributes[fld] == 1234
 
+# TODO
 
-@pytest.mark.parametrize(
-	"test_entry",
-	(
-		"f_object_entry_user",
-		"f_object_entry_group",
-	),
-)
-def test_ddr_fetch_object(
-	test_entry, request: FixtureRequest, f_connection, f_object_args
-):
-	m_entry = request.getfixturevalue(test_entry)()
-	f_connection.entries = [m_entry]
+def test_ddr_sync_object_no_entry(mocker: MockerFixture):
+	mocker.patch.object(LDAPObject, "__init__", return_value=None)
+	m_object = LDAPObject()
+	assert m_object.__sync_object__() is None
 
-	object_args = f_object_args()
-	m_ldap_object = LDAPObject(**object_args)
-	f_connection.search.assert_called_once_with(
-		search_base=m_ldap_object.search_base,
-		search_filter=m_ldap_object.search_filter,
-		search_scope="SUBTREE",
-		attributes=m_ldap_object.search_attrs,
-	)
-	m_ldap_object.__fetch_object__() == m_entry
+def test_ddr_sync_object(mocker: MockerFixture, f_object_entry_user: LDAPEntry):
+	mocker.patch.object(LDAPObject, "__init__", return_value=None)
+	m_object = LDAPObject()
+	m_object.entry = f_object_entry_user()
+	assert m_object.__sync_object__() is None
+	assert m_object.attributes
 
-
-def test_ddr_fetch_object_removes_empty_string(
-	f_object_attrs_user,
-	f_object_entry_user,
-	f_connection,
-	f_object_args,
-):
-	m_entry = f_object_entry_user(attrs={"objectRid": ""})
-	result: dict = deepcopy(f_object_attrs_user())
-	result.pop("objectRid")
-
-	f_connection.entries = [m_entry]
-
-	object_args = f_object_args()
-	m_ldap_object = LDAPObject(**object_args)
-	f_connection.search.assert_called_once_with(
-		search_base=m_ldap_object.search_base,
-		search_filter=m_ldap_object.search_filter,
-		search_scope="SUBTREE",
-		attributes=m_ldap_object.search_attrs,
-	)
-	m_ldap_object.__fetch_object__() == result
-
-
-@pytest.mark.parametrize(
-	"object_classes",
-	(
-		"builtinDomain",  # single value
-		["builtinDomain", "someOtherValue"],  # iterable
-	),
-)
-def test_ddr_fetch_object_marks_builtin(
-	object_classes,
-	f_object_entry_user,
-	f_connection,
-	f_object_args,
-):
-	m_entry: dict = f_object_entry_user(attrs={"objectClass": object_classes})
-	f_connection.entries = [m_entry]
-
-	object_args = f_object_args()
-	m_ldap_object = LDAPObject(**object_args)
-	f_connection.search.assert_called_once_with(
-		search_base=m_ldap_object.search_base,
-		search_filter=m_ldap_object.search_filter,
-		search_scope="SUBTREE",
-		attributes=m_ldap_object.search_attrs,
-	)
-	assert m_ldap_object.attributes["builtin"] is True
-
-
-def test_ddr_fetch_object_no_results_returns_none(
-	f_object_args, f_connection
-):
-	# Setup
-	object_args = f_object_args()
-	f_connection.entries = []
-
-	# Instantiate LDAPObject
-	ldap_obj = LDAPObject(**object_args)
-
-	# Execute
-	result = ldap_obj.__fetch_object__()
-
-	# Assert
-	assert result is None
-	assert ldap_obj.attributes is None
-	assert ldap_obj.entry is None
-
-
-def test_ddr_fetch_object_handles_sid_conversion(
-	mocker, f_object_args, f_object_entry_user, f_connection
-):
-	# Setup
-	object_args = f_object_args()
-	mock_entry = f_object_entry_user()
-	f_connection.entries = [mock_entry]
-
-	# Mock SID conversion
-	mock_sid = mocker.MagicMock(spec=SID)
-	mock_sid.__str__.return_value = (
-		"S-1-5-21-123456789-1234567890-123456789-1105"
-	)
-	mocker.patch("core.models.ldap_object.SID", return_value=mock_sid)
-
-	# Instantiate LDAPObject
-	ldap_obj = LDAPObject(**object_args)
-
-	# Execute
-	ldap_obj.__fetch_object__()
-
-	# Assert SID was processed
-	assert (
-		ldap_obj.attributes["objectSid"]
-		== "S-1-5-21-123456789-1234567890-123456789-1105"
-	)
-	assert ldap_obj.attributes["objectRid"] == 1105
-
-
-def test_ddr_fetch_object_handles_iterable_attributes(
-	f_object_args, f_object_entry_user, f_connection
-):
-	# Setup
-	object_args = f_object_args()
-	mock_entry = f_object_entry_user(
-		attrs={"objectClass": ["value1", "value2"]}
-	)
-
-	# Add an iterable attribute
-	f_connection.entries = [mock_entry]
-	object_args["ldap_attrs"] = DEFAULT_REQUIRED_LDAP_ATTRS + ["objectClass"]
-
-	# Instantiate LDAPObject
-	ldap_obj = LDAPObject(**object_args)
-
-	# Execute
-	ldap_obj.__fetch_object__()
-
-	# Assert iterable attribute was handled correctly
-	assert ldap_obj.attributes["objectClass"] == ["value1", "value2"]
-
-
-def test_ddr_ldap_attrs_returns_attribute_keys(
-	f_object_args, f_object_entry_user, f_connection
-):
-	# Setup
-	object_args = f_object_args()
-	mock_entry = f_object_entry_user()
-	f_connection.entries = [mock_entry]
-
-	# Instantiate and fetch object
-	ldap_obj = LDAPObject(**object_args)
-	ldap_obj.__fetch_object__()
-
-	# Execute
-	result = ldap_obj.__ldap_attrs__()
-
-	# Assert
-	assert isinstance(result, list)
-	assert all(attr in result for attr in ldap_obj.attributes.keys())
-
-
-def test_ddr_ldap_attrs_returns_empty_list_when_no_attributes(
-	f_object_args, f_connection
-):
-	# Setup
-	object_args = f_object_args()
-
-	# Instantiate without fetching
-	ldap_obj = LDAPObject(**object_args)
-
-	# Execute
-	result = ldap_obj.__ldap_attrs__()
-
-	# Assert
-	assert result == []
-
+# test fetch
+# test ldap_attrs
+# test get_local_alias_for_ldap_key
+# test value_changed
+# test create
+# test update
+# test delete
+# test save
 
 def test_ddr_get_common_name_extracts_cn_from_dn(
 	f_object_args, f_connection
