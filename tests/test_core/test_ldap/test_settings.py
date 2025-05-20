76f7ffddb3c88e6be30654fd570de372730b061e
Dylan Blanque <dylan.blanque@gmail.com>
2025-05-20 20:17:25 -0300

    Add stub file for core.ldap.ldap_settings tests

---
+++ tests/test_core/test_ldap/test_settings.py
@@ -0,0 +1,6 @@
+########################### Standard Pytest Imports ############################
+import pytest
+from pytest import FixtureRequest
+from pytest_mock import MockerFixture, MockType
+################################################################################
+
