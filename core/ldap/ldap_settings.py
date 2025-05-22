2382a486c6a4bc23170cd329718f65df2fff9b5b
Dylan Blanque <dylan.blanque@gmail.com>
2025-05-22 14:18:09 -0300

    Minor fixes to get_setting_list code order and docstring.

--- core/ldap/ldap_settings.py
+++ core/ldap/ldap_settings.py
@@ -15,6 +15,11 @@ from enum import Enum
 from django.core.exceptions import ObjectDoesNotExist
 from interlock_backend.settings import DEFAULT_SUPERUSER_USERNAME
 from core.ldap import defaults
+from core.constants.attrs.local import LOCAL_ATTR_VALUE, LOCAL_ATTR_TYPE
+from core.constants.settings import (
+	K_LDAP_AUTH_TLS_VERSION,
+	K_LDAP_AUTH_CONNECTION_PASSWORD,
+)
 from interlock_backend.encrypt import aes_decrypt
 
 # Full imports
@@ -24,19 +29,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def get_setting_list(preset_id: int = 1):
-	"""Returns a Dictionary with the current setting values in the system
-
-	Arguments:
-
-	settingList (STRING) - Default is CMAPS
-
-	listFormat (STRING) - frontend or backend
-
-	BACKEND - Returns Object
-	FRONTEND - Returns Dict with values and types
-	"""
-
+def get_setting_list(preset_id: int = 1) -> dict[dict]:
+	"""Returns a Dictionary with the current setting values in the system"""
 	data = {}
 	userQuerySet = User.objects.filter(username=DEFAULT_SUPERUSER_USERNAME)
 	if userQuerySet.count() > 0:
@@ -49,32 +43,31 @@ def get_setting_list(preset_id: int = 1):
 	for setting_key, setting_type in LDAP_SETTING_MAP.items():
 		setting_instance = None
 		data[setting_key] = {}
-		data[setting_key]["type"] = setting_type.lower()
+		data[setting_key][LOCAL_ATTR_TYPE] = setting_type.lower()
 		try:
 			setting_instance = LDAPSetting.objects.get(
 				preset_id=preset_id, name=setting_key
 			)
-		except ObjectDoesNotExist:
-			data[setting_key]["value"] = getattr(defaults, setting_key)
-			if setting_key == "LDAP_AUTH_TLS_VERSION" and isinstance(
-				data[setting_key]["value"], Enum
-			):
-				data[setting_key]["value"] = data[setting_key]["value"].name
-			continue
 
-		if setting_key == "LDAP_AUTH_CONNECTION_PASSWORD":
-			try:
-				data[setting_key]["value"] = aes_decrypt(
-					*setting_instance.value
-				)
-			except:
-				data[setting_key]["value"] = ""
-				print("Could not decrypt password")
-				pass
-		else:
-			data[setting_key]["value"] = setting_instance.value
-			if setting_key == "LDAP_AUTH_TLS_VERSION" and isinstance(
-				setting_instance.value, Enum
+			if setting_key == K_LDAP_AUTH_CONNECTION_PASSWORD:
+				try:
+					data[setting_key][LOCAL_ATTR_VALUE] = aes_decrypt(
+						*setting_instance.value
+					)
+				except:
+					data[setting_key][LOCAL_ATTR_VALUE] = ""
+					logger.error("Could not decrypt password")
+					pass
+			else:
+				data[setting_key][LOCAL_ATTR_VALUE] = setting_instance.value
+				if setting_key == K_LDAP_AUTH_TLS_VERSION and isinstance(
+					setting_instance.value, Enum
+				):
+					data[setting_key][LOCAL_ATTR_VALUE] = setting_instance.value.name
+		except ObjectDoesNotExist:
+			data[setting_key][LOCAL_ATTR_VALUE] = getattr(defaults, setting_key)
+			if setting_key == K_LDAP_AUTH_TLS_VERSION and isinstance(
+				data[setting_key][LOCAL_ATTR_VALUE], Enum
 			):
-				data[setting_key]["value"] = setting_instance.value.name
+				data[setting_key][LOCAL_ATTR_VALUE] = data[setting_key][LOCAL_ATTR_VALUE].name
 	return data
