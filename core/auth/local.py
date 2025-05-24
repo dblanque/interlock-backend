27df2cf0f7b0500c1ebca6580272012e3e79efab
Dylan Blanque <dylan.blanque@gmail.com>
2025-05-24 14:22:41 -0300

    Minor fixes to EmailAuthBackend.

--- core/auth/local.py
+++ core/auth/local.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
-
-class EmailAuthBackend(object):
+class EmailAuthBackend(ModelBackend):
 	"""
 	Authenticate Local DB user using an e-mail address.
 	"""
+	supports_inactive_user = False
 
 	def authenticate(self, request, username=None, password=None):
 		User = get_user_model()
