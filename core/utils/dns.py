fc7b8d06602a581cc7af08fbd19725b8bf472c36
Dylan Blanque <dylan.blanque@gmail.com>
2025-05-29 19:30:53 -0300

    Change function name to snake_case.

--- core/utils/dns.py
+++ core/utils/dns.py
@@ -1,50 +1,7 @@
-import dns.asyncbackend
-import dns.asyncquery
-import dns.asyncresolver
-import dns.dnssec
-import dns.e164
-import dns.edns
-import dns.entropy
-import dns.exception
-import dns.flags
-import dns.immutable
-import dns.inet
-import dns.ipv4
-import dns.ipv6
-import dns.message
-import dns.name
-import dns.namedict
-import dns.node
-import dns.opcode
-import dns.query
-import dns.rcode
-import dns.rdata
-import dns.rdataclass
-import dns.rdataset
-import dns.rdatatype
-import dns.renderer
 import dns.resolver
-import dns.reversename
-import dns.rrset
-import dns.serial
-import dns.set
-import dns.tokenizer
-import dns.transaction
-import dns.tsig
-import dns.tsigkeyring
-import dns.ttl
-import dns.rdtypes
-import dns.update
-import dns.version
-import dns.versioned
-import dns.wire
-import dns.xfr
-import dns.zone
-import dns.zonefile
 import ipaddress
 
-
-def getDNSResolver(dnsAddresses):
+def get_dns_resolver(dnsAddresses):
 	# Set Query for IP Address
 	if dnsAddresses is None:
 		raise ValueError("You need a Server Address to query")
