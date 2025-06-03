34fe2db180d060c6415c91cf391b4401410f86cc
Dylan Blanque <dylan.blanque@gmail.com>
2025-06-03 16:31:59 -0300

    Add stub test file.

---
+++ tests/test_core/test_views/test_user.py
@@ -0,0 +1,32 @@
+########################### Standard Pytest Imports ############################
+import pytest
+from pytest import FixtureRequest
+from pytest_mock import MockerFixture, MockType
+################################################################################
+
+class TestList:
+	pass
+
+class TestInsert:
+	pass
+
+class TestFetch:
+	pass
+
+class TestUpdate:
+	pass
+
+class TestDelete:
+	pass
+
+class TestChangeStatus:
+	pass
+
+class TestChangePassword:
+	pass
+
+class TestSelfChangePassword:
+	pass
+
+class TestSelfUpdate:
+	pass
