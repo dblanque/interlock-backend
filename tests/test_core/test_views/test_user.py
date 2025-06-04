bc02e968f68064aa07651fca2c340ae65a1caa8c
Dylan Blanque <dylan.blanque@gmail.com>
2025-06-04 16:19:22 -0300

    Add tests for Fetch, Update, Delete, Change Status endpoints.

--- tests/test_core/test_views/test_user.py
+++ tests/test_core/test_views/test_user.py
@@ -2,9 +2,11 @@
 import pytest
 from pytest import FixtureRequest
 from pytest_mock import MockerFixture, MockType
-
 ################################################################################
-from django.urls import reverse
+from tests.test_core.test_views.conftest import (
+	BaseViewTestClass,
+	BaseViewTestClassWithPk,
+)
 from rest_framework.test import APIClient
 from rest_framework.response import Response
 from rest_framework import status
@@ -21,6 +23,7 @@ from core.constants.attrs.local import (
 	LOCAL_ATTR_PASSWORD_CONFIRM,
 	LOCAL_ATTR_EMAIL,
 	LOCAL_ATTR_IS_ENABLED,
+	LOCAL_ATTR_ENABLED,
 )
 from core.models.choices.log import (
 	LOG_CLASS_USER,
@@ -28,7 +31,10 @@ from core.models.choices.log import (
 	LOG_ACTION_UPDATE,
 	LOG_ACTION_READ,
 	LOG_ACTION_DELETE,
+	LOG_EXTRA_ENABLE,
+	LOG_EXTRA_DISABLE,
 )
+from core.constants.user import LOCAL_PUBLIC_FIELDS
 
 @pytest.fixture(autouse=True)
 def f_ldap_connector(g_ldap_connector: ConnectorFactory):
@@ -41,9 +47,12 @@ def f_user_test(user_factory: UserFactory) -> User:
 		email="mockuser@example.com",
 	)
 
+@pytest.fixture
+def f_log(mocker: MockerFixture):
+	return mocker.patch("core.views.user.DBLogMixin.log")
 
-class TestList:
-	endpoint = reverse("users-list")
+class TestList(BaseViewTestClass):
+	_endpoint = "users-list"
 
 	def test_success(
 		self,
@@ -71,8 +80,8 @@ class TestList:
 		)
 
 
-class TestInsert:
-	endpoint = reverse("users-insert")
+class TestInsert(BaseViewTestClass):
+	_endpoint = "users-insert"
 
 	@pytest.mark.parametrize(
 		"with_password",
@@ -87,6 +96,7 @@ class TestInsert:
 		admin_user_client: APIClient,
 		mocker: MockerFixture,
 		g_interlock_ldap_disabled,
+		f_log: MockType,
 	):
 		m_data = {
 			LOCAL_ATTR_USERNAME: "new_user",
@@ -99,7 +109,6 @@ class TestInsert:
 			m_password = m_data.pop(LOCAL_ATTR_PASSWORD)
 			del m_data[LOCAL_ATTR_PASSWORD_CONFIRM]
 
-		m_log = mocker.patch("core.views.user.DBLogMixin.log")
 		m_ldap_user_exists = mocker.patch(
 			"core.views.user.UserViewSet.ldap_user_exists"
 		)
@@ -124,7 +133,7 @@ class TestInsert:
 			assert not user.check_password("")
 
 		# Verify logging
-		m_log.assert_called_once_with(
+		f_log.assert_called_once_with(
 			user=mocker.ANY,  # request.user.id
 			operation_type=LOG_ACTION_CREATE,
 			log_target_class=LOG_CLASS_USER,
@@ -133,10 +142,11 @@ class TestInsert:
 
 	def test_success_with_ldap_enabled(
 		self,
+		mocker: MockerFixture,
 		f_ldap_connector: LDAPConnectorMock,
 		admin_user_client: APIClient,
-		mocker: MockerFixture,
 		g_interlock_ldap_enabled,
+		f_log: MockType,
 	):
 		user_data = {
 			LOCAL_ATTR_USERNAME: "ldap_user",
@@ -148,7 +158,6 @@ class TestInsert:
 		m_ldap_user_exists = mocker.patch(
 			"core.views.user.UserViewSet.ldap_user_exists", return_value=False
 		)
-		m_log = mocker.patch("core.views.user.DBLogMixin.log")
 
 		response: Response = admin_user_client.post(self.endpoint, user_data)
 
@@ -166,7 +175,7 @@ class TestInsert:
 		assert user.email == user_data[LOCAL_ATTR_EMAIL]
 
 		# Verify logging
-		m_log.assert_called_once_with(
+		f_log.assert_called_once_with(
 			user=mocker.ANY,
 			operation_type=LOG_ACTION_CREATE,
 			log_target_class=LOG_CLASS_USER,
@@ -175,8 +184,8 @@ class TestInsert:
 
 	def test_serializer_validation_failure(
 		self,
-		admin_user_client: APIClient,
 		mocker: MockerFixture,
+		admin_user_client: APIClient,
 	):
 		"""Test invalid payload returns BadRequest"""
 		invalid_data = {
@@ -198,8 +207,8 @@ class TestInsert:
 
 	def test_serializer_password_validation_failure(
 		self,
-		admin_user_client: APIClient,
 		mocker: MockerFixture,
+		admin_user_client: APIClient,
 	):
 		"""Test invalid payload returns BadRequest"""
 		invalid_data = {
@@ -221,8 +230,8 @@ class TestInsert:
 
 	def test_ldap_user_exists_raises_error(
 		self,
-		admin_user_client: APIClient,
 		mocker: MockerFixture,
+		admin_user_client: APIClient,
 		f_ldap_connector: LDAPConnectorMock,
 		g_interlock_ldap_enabled,
 	):
@@ -252,17 +261,299 @@ class TestInsert:
 			username=user_data[LOCAL_ATTR_USERNAME]
 		).exists()
 
-# class TestFetch:
-# 	endpoint = reverse("users-fetch")
+class TestFetch(BaseViewTestClassWithPk):
+	_endpoint = "users-fetch"
 
-# class TestUpdate:
-# 	endpoint = reverse("users-update")
+	def test_success(
+		self,
+		admin_user_client: APIClient,
+		admin_user: User,
+		f_user_test: User,
+		f_log: MockType,
+	):
+		self._pk = f_user_test.id
 
-# class TestDelete:
-# 	endpoint = reverse("users-delete")
+		response: Response = admin_user_client.get(self.endpoint)
+		response_data: dict = response.data.get("data")
 
-# class TestChangeStatus:
-# 	endpoint = reverse("users-change-status")
+		assert response.status_code == status.HTTP_200_OK
+		for fld in LOCAL_PUBLIC_FIELDS:
+			assert fld in response_data
+			assert response_data[fld] == getattr(f_user_test, fld)
+		f_log.assert_called_once_with(
+			user=admin_user.id,
+			operation_type=LOG_ACTION_READ,
+			log_target_class=LOG_CLASS_USER,
+			log_target=f_user_test.username,
+		)
+
+	def test_raises_user_not_exist(
+		self,
+		admin_user_client: APIClient,
+		f_log: MockType,
+	):
+		self._pk = 999
+
+		response: Response = admin_user_client.get(self.endpoint)
+
+		assert response.status_code == status.HTTP_404_NOT_FOUND
+		f_log.assert_not_called()
+
+
+class TestUpdate(BaseViewTestClass):
+	_endpoint = "users-list"
+
+	def test_success(
+		self,
+		admin_user_client: APIClient,
+		admin_user: User,
+		f_user_test: User,
+		f_log: MockType,
+	):
+		m_password = "newpassword123"
+
+		response: Response = admin_user_client.put(
+			self.endpoint + f"{f_user_test.id}/",
+			data={
+				LOCAL_ATTR_DN: "some_dn",
+				LOCAL_ATTR_EMAIL: "new.email@example.com",
+				LOCAL_ATTR_PASSWORD: m_password,
+				LOCAL_ATTR_PASSWORD_CONFIRM: m_password,
+			},
+			format="json",
+		)
+
+		assert response.status_code == status.HTTP_200_OK
+
+		f_user_test.refresh_from_db()
+		assert f_user_test.email == "new.email@example.com"
+		assert f_user_test.check_password(m_password)
+		f_log.assert_called_once_with(
+			user=admin_user.id,
+			operation_type=LOG_ACTION_UPDATE,
+			log_target_class=LOG_CLASS_USER,
+			log_target=f_user_test.username,
+		)
+
+	def test_raises_user_not_exists(
+		self,
+		mocker: MockerFixture,
+		admin_user_client: APIClient,
+		f_user_test: User,
+		f_log: MockType,
+	):
+		m_save = mocker.patch.object(User, "save")
+		response: Response = admin_user_client.put(
+			self.endpoint + f"999/",
+			data={LOCAL_ATTR_EMAIL: "new.email@example.com"},
+			format="json",
+		)
+
+		assert response.status_code == status.HTTP_404_NOT_FOUND
+
+		f_user_test.refresh_from_db()
+		assert f_user_test.email != "new.email@example.com"
+		m_save.assert_not_called()
+		f_log.assert_not_called()
+
+	def test_raises_user_not_local(
+		self,
+		mocker: MockerFixture,
+		admin_user_client: APIClient,
+		f_user_ldap: User,
+		f_log: MockType,
+	):
+		m_save = mocker.patch.object(User, "save")
+		response: Response = admin_user_client.put(
+			self.endpoint + f"{f_user_ldap.id}/",
+			data={LOCAL_ATTR_EMAIL: "new.email@example.com"},
+			format="json",
+		)
+
+		assert response.status_code == status.HTTP_406_NOT_ACCEPTABLE
+
+		f_user_ldap.refresh_from_db()
+		assert f_user_ldap.email != "new.email@example.com"
+		m_save.assert_not_called()
+		f_log.assert_not_called()
+
+	def test_raises_serializer_error(
+		self,
+		mocker: MockerFixture,
+		admin_user_client: APIClient,
+		f_user_test: User,
+		f_log: MockType,
+	):
+		m_save = mocker.patch.object(User, "save")
+		m_password = "newpasswordtotest"
+
+		response: Response = admin_user_client.put(
+			self.endpoint + f"{f_user_test.id}/",
+			data={
+				LOCAL_ATTR_EMAIL: "bad-email",
+				LOCAL_ATTR_PASSWORD: m_password,
+			},
+		)
+		response_errors = response.data.get("errors")
+
+		assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+		f_user_test.refresh_from_db()
+		m_save.assert_not_called()
+		f_log.assert_not_called()
+		assert LOCAL_ATTR_EMAIL in response_errors
+		# Password confirm validation is done after Model fields.
+		assert LOCAL_ATTR_PASSWORD not in response_errors
+		assert not f_user_test.check_password(m_password)
+
+
+class TestDelete(BaseViewTestClassWithPk):
+	_endpoint = "users-delete"
+
+	def test_success(
+		self,
+		admin_user_client: APIClient,
+		admin_user: User,
+		f_user_test: User,
+		f_log: MockType,
+	):
+		self._pk = f_user_test.id
+
+		response: Response = admin_user_client.delete(self.endpoint)
+		assert response.status_code == status.HTTP_200_OK
+		assert not User.objects.filter(username=f_user_test.username).exists()
+		f_log.assert_called_once_with(
+			user=admin_user.id,
+			operation_type=LOG_ACTION_DELETE,
+			log_target_class=LOG_CLASS_USER,
+			log_target=f_user_test.username,
+		)
+
+	def test_raises_not_exists(
+		self,
+		mocker: MockerFixture,
+		admin_user_client: APIClient,
+		f_log: MockType,
+	):
+		m_delete_permanently = mocker.patch.object(User, "delete_permanently")
+		self._pk = 999
+
+		response: Response = admin_user_client.delete(self.endpoint)
+		assert response.status_code == status.HTTP_404_NOT_FOUND
+		m_delete_permanently.assert_not_called()
+		f_log.assert_not_called()
+
+	def test_raises_anti_lockout(
+		self,
+		mocker: MockerFixture,
+		admin_user_client: APIClient,
+		admin_user: User,
+		f_log: MockType,
+	):
+		m_delete_permanently = mocker.patch.object(User, "delete_permanently")
+		self._pk = admin_user.id
+
+		response: Response = admin_user_client.delete(self.endpoint)
+		assert response.status_code == status.HTTP_400_BAD_REQUEST
+		assert User.objects.filter(username=admin_user.username).exists()
+		m_delete_permanently.assert_not_called()
+		f_log.assert_not_called()
+
+	def test_raises_not_local(
+		self,
+		mocker: MockerFixture,
+		admin_user_client: APIClient,
+		f_user_ldap: User,
+		f_log: MockType,
+	):
+		m_delete_permanently = mocker.patch.object(User, "delete_permanently")
+		self._pk = f_user_ldap.id
+
+		response: Response = admin_user_client.delete(self.endpoint)
+		assert response.status_code == status.HTTP_406_NOT_ACCEPTABLE
+		assert User.objects.filter(username=f_user_ldap.username).exists()
+		m_delete_permanently.assert_not_called()
+		f_log.assert_not_called()
+
+
+class TestChangeStatus(BaseViewTestClassWithPk):
+	_endpoint = "users-change-status"
+
+	@pytest.mark.parametrize(
+		"enabled, mock_opposite",
+		(
+			(True, True),
+			(False, True),
+			(True, False),
+			(False, False),
+		),
+	)
+	def test_success(
+		self,
+		enabled: bool,
+		mock_opposite: bool,
+		admin_user_client: APIClient,
+		admin_user: User,
+		f_user_test: User,
+		f_log: MockType,	
+	):
+		self._pk = f_user_test.id
+
+		# Mock opposite value
+		if mock_opposite:
+			f_user_test.is_enabled = (not enabled)
+			f_user_test.save()
+
+		response: Response = admin_user_client.post(
+			self.endpoint,
+			data={LOCAL_ATTR_ENABLED: enabled},
+			format="json",
+		)
+		assert response.status_code == status.HTTP_200_OK
+		f_user_test.refresh_from_db()
+		assert f_user_test.is_enabled == enabled
+		f_log.assert_called_once_with(
+			user=admin_user.id,
+			operation_type=LOG_ACTION_UPDATE,
+			log_target_class=LOG_CLASS_USER,
+			log_target=f_user_test.username,
+			message=LOG_EXTRA_ENABLE if enabled else LOG_EXTRA_DISABLE,
+		)
+
+	def test_raises_not_local(
+		self,
+		mocker: MockerFixture,
+		admin_user_client: APIClient,
+		f_user_ldap: User,
+		f_log: MockType,	
+	):
+		m_save = mocker.patch.object(User, "save")
+		self._pk = f_user_ldap.id
+		response: Response = admin_user_client.post(
+			self.endpoint,
+			data={LOCAL_ATTR_ENABLED: True},
+			format="json",
+		)
+		assert response.status_code == status.HTTP_406_NOT_ACCEPTABLE
+		m_save.assert_not_called()
+		f_log.assert_not_called()
+
+	def test_raises_not_exists(
+		self,
+		mocker: MockerFixture,
+		admin_user_client: APIClient,
+		f_log: MockType,	
+	):
+		m_save = mocker.patch.object(User, "save")
+		self._pk = 999
+		response: Response = admin_user_client.post(
+			self.endpoint,
+			data={LOCAL_ATTR_ENABLED: True},
+			format="json",
+		)
+		assert response.status_code == status.HTTP_404_NOT_FOUND
+		m_save.assert_not_called()
+		f_log.assert_not_called()
 
 # class TestChangePassword:
 # 	endpoint = reverse("users-change-password")
