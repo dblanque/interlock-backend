37c9c0183d3ff5b291be13ba7fb55adebec348d5
Dylan Blanque <dylan.blanque@gmail.com>
2025-06-12 12:34:30 -0300

    Modify conditional to use exists instead of count.

--- core/setup/user.py
+++ core/setup/user.py
@@ -6,8 +6,8 @@ from core.utils.db import db_table_exists
 def create_default_superuser():
 	if not db_table_exists("core_user"):
 		return
-	if User.objects\
+	if not User.objects\
 		.get_full_queryset()\
 		.filter(username=DEFAULT_SUPERUSER_USERNAME)\
-		.count() == 0:
+		.exists():
 		User.objects.create_default_superuser()
