acfa0df16d55df0fd1186e420bdee5b276cdcf54
Dylan Blanque <dylan.blanque@gmail.com>
2025-06-13 17:36:45 -0300

    Minor fixes.

--- interlock_backend/server_timezone.py
+++ interlock_backend/server_timezone.py
@@ -1,9 +1,14 @@
 import os
 import pytz
 import subprocess
+from logging import getLogger
+logger = getLogger()
 
+# Local Time Path
+_LTP = "/etc/localtime"
 
 def get_server_timezone():
+	"""Gets server localtime timezone."""
 	time_zone = None
 	try:
 		# timedatectl (most Linux systems)
@@ -16,24 +21,25 @@ def get_server_timezone():
 			.strip()
 		)
 	except:
+		# Linux / macOS only (Windows unsupported)
 		try:
-			# Linux / macOS only (Windows unsupported)
-			try:
-				time_zone = os.path.realpath("/etc/localtime").split("/")[-2:][
-					1
-				]
-			except:
-				pass
+			time_zone_path = os.path.realpath(_LTP)
+			if "zoneinfo" in time_zone_path:
+				time_zone = time_zone_path.split("zoneinfo/")[-1]
+			else:
+				time_zone = "/".join(time_zone_path.split("/")[-2:])
 		except:
 			# Fallback to UTC
 			time_zone = "UTC"
+			pass
 
 	# Validate the timezone exists in pytz
 	if time_zone not in pytz.all_timezones:
-		print(
-			f"Warning: Detected timezone '{time_zone}' is invalid. Falling back to UTC."
+		logger.warning(
+			f"Warning: Detected timezone '{time_zone}' is invalid. "
+			"Falling back to UTC."
 		)
 		time_zone = "UTC"
 
-	print(f"Configured timezone: {time_zone}")
+	logger.info(f"Configured timezone: {time_zone}")
 	return time_zone
