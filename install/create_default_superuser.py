6c2bb4271a3a2050541f7a7ebfbc32e0a3538d54
Dylan Blanque <dylan.blanque@gmail.com>
2025-06-24 16:25:17 -0300

    Fixes to default create superuser shell util.

--- install/create_default_superuser.py
+++ install/create_default_superuser.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
 from interlock_backend.settings import DEFAULT_SUPERUSER_USERNAME
 
 User = get_user_model()
-if User.objects.filter(username=DEFAULT_SUPERUSER_USERNAME).count() == 0:
+try:
+	User.objects.get(username=DEFAULT_SUPERUSER_USERNAME)
+except ObjectDoesNotExist:
 	User.objects.create_default_superuser()
