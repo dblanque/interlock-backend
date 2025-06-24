cad13a098a1fa66a66f2990d0a8483dbc4896601
Dylan Blanque <dylan.blanque@gmail.com>
2025-06-24 16:24:17 -0300

    Rollback previous commit.

--- core/setup/user.py
+++ core/setup/user.py
@@ -1,13 +1,14 @@
 from core.models.user import User
 from interlock_backend.settings import DEFAULT_SUPERUSER_USERNAME
 from core.utils.db import db_table_exists
-from django.core.exceptions import ObjectDoesNotExist
 
 
 def create_default_superuser():
 	if not db_table_exists("core_user"):
 		return
-	try:
-		User.objects.get(username=DEFAULT_SUPERUSER_USERNAME)
-	except ObjectDoesNotExist:
+	if (
+		not User.objects.get_full_queryset()
+		.filter(username=DEFAULT_SUPERUSER_USERNAME)
+		.exists()
+	):
 		User.objects.create_default_superuser()
