3e98d3ebd302247ff8bfe69f2506e0ced7da6e2a
Dylan Blanque <dylan.blanque@gmail.com>
2025-06-24 15:15:09 -0300

    Remove unrequired CORS value settings from sample file.

--- interlock_backend/local_django_settings_sample.py
+++ interlock_backend/local_django_settings_sample.py
@@ -17,17 +17,19 @@ DATABASES = {
 }
 
 FRONT_URL = "interlock.example.com"
-CORS_ALLOWED_ORIGINS = [
-	# ! DEV
-	"http://127.0.0.1",
-	# PRODUCTION
-	"http://interlock.example.com",
-	"https://interlock.example.com",
-]
-CSRF_TRUSTED_ORIGINS = [
-	# ! DEV
-	"http://127.0.0.1",
-	# PRODUCTION
-	"http://interlock.example.com",
-	"https://interlock.example.com",
-]
+
+# CORS and CSRF overrides
+# CORS_ALLOWED_ORIGINS = [
+# 	# ! DEV
+# 	"http://127.0.0.1",
+# 	# PRODUCTION
+# 	f"http://{FRONT_URL}",
+# 	f"https://{FRONT_URL}",
+# ]
+# CSRF_TRUSTED_ORIGINS = [
+# 	# ! DEV
+# 	"http://127.0.0.1",
+# 	# PRODUCTION
+# 	f"http://{FRONT_URL}",
+# 	f"https://{FRONT_URL}",
+# ]
